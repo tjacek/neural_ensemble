b049eee92f46b1f8793ff2e93397c90cff89fe8b
tjacek <tjacek@agh.edu.pl>
2023-02-19 22:11:39 +0100

    format data

--- output.py
+++ output.py
@@ -1,3 +1,4 @@
+import numpy as np
 import ens,utils
 
 class ESCFExp(object):
@@ -9,9 +10,8 @@ class ESCFExp(object):
         self.ens_types=ens_types
         self.clf_types=clf_types
 
-    @utils.dir_fun(False)
+    @utils.dir_fun(as_dict=True)
     def __call__(self,in_path):
-       
         @utils.dir_fun(as_dict=False)
         @utils.unify_cv(dir_path=None)
         def helper(path_i):
@@ -22,11 +22,30 @@ class ESCFExp(object):
                     ens_i=alg_i(common,binary,clf_j)
                     result_i=ens_i.evaluate()
                     id_ij=",".join([str(ens_i),clf_j])
+                    print(id_ij)
                     result_dict[id_ij]=result_i
             return result_dict
-        acc=helper(in_path)
-        raise Exception(acc)
-        print((alg_i,clf_j,acc))
+        acc=helper(in_path)    
+        exp={ id_i:[acc_j[id_i] for acc_j in acc] 
+                for id_i in acc[0].keys()}
+        lines=[ f'{id_i},{stats(acc_i,True)}' 
+            for id_i,acc_i in exp.items()]
+        return lines
+
+def stats(acc,as_str=False):
+    raw=[f'{fun_i(acc):.4f}' 
+        for fun_i in [np.mean,np.std]]
+    if(as_str):
+        return ','.join(raw)
+    return raw
+
+def format(line_dict):
+    lines=[]
+    for data_i,lines_i in line_dict.items():
+        for line_j in lines_i:
+            lines.append(f'{data_i},{line_j}')
+    print('\n'.join(lines))
 
 exp=ESCFExp()
-exp('test')
+line_dict=exp('test')
+format(line_dict)
