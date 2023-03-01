8942bf7ba9e4ae689c4fd26090e78104dc413026
tjacek <tjacek@agh.edu.pl>
2023-03-01 01:55:22 +0100

    binary module

---
+++ binary.py
@@ -0,0 +1,86 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from itertools import combinations
+import tensorflow as tf
+import numpy as np
+import data,nn,learn
+
+class NeuralEnsemble(BaseEstimator, ClassifierMixin):
+    def __init__(self,n_hidden=250,n_epochs=200):
+        self.n_hidden=n_hidden
+        self.n_epochs=n_epochs
+
+    def fit(self,X,targets):
+        raise NotImplementedError
+
+    def predict_proba(self,X):
+        votes=[]
+        for extr_i,model_i in zip(self.extractors,self.models):
+            binary_i=extr_i.predict(X)
+            full_i=np.concatenate([X,binary_i],axis=1)
+            vote_i= model_i.predict_proba(full_i)
+            votes.append(vote_i)
+        votes=np.array(votes)
+        prob=np.sum(votes,axis=0)
+        return prob
+
+    def predict(self,X):
+        prob=self.predict_proba(X)
+        return np.argmax(prob,axis=1)
+
+    def save(self,out_path):
+        data.make_dir(out_path)
+        for i,extr_i in enumerate(self.extractors):
+            extr_i.save(f'{out_path}/{i}')
+
+    def train_extractor(self,X,y_i):
+        nn_params={'dims':X.shape[1],'n_cats':2}
+        nn_i=nn.SimpleNN(n_hidden=self.n_hidden)(nn_params)
+        nn_i.fit(X,y_i,
+                epochs=self.n_epochs,batch_size=32)
+        extractor_i= nn.get_extractor(nn_i)
+        self.extractors.append(extractor_i)
+        return extractor_i
+
+    def train_model(self,X,binary_i,targets):
+        clf_i=learn.get_clf('LR')
+        full_i=np.concatenate([X,binary_i],axis=1)
+        clf_i.fit(full_i,targets)
+        self.models.append(clf_i)
+
+class OneVsAll(NeuralEnsemble):
+    def fit(self,X,targets):
+        self.extractors=[]
+        self.models=[]
+        n_cats=max(targets)+1
+        for cat_i in range(n_cats):
+            y_i=binarize(cat_i,targets)
+            extractor_i=self.train_extractor(X,y_i)
+            binary_i=extractor_i.predict(X)
+            self.train_model(X,binary_i,targets)
+
+class OneVsOne(NeuralEnsemble):
+    def fit(self,X,targets):
+        self.extractors=[]
+        self.models=[]
+        n_cats=max(targets)+1
+        pairs = list(combinations(range(n_cats),2))
+        for i,j in pairs:
+            selected=[k for k,y_k in enumerate(targets)
+                        if((y_k==i) or (y_k==j))]
+            y_s=[ int(targets[k]==i) for k in selected]                
+            y_s= tf.keras.utils.to_categorical(y_s, num_classes = 2)
+            X_s=np.array([ X[k,:] for k in selected])
+            extractor_i=self.train_extractor(X_s,y_s)
+            binary_i=extractor_i.predict(X)
+            self.train_model(X,binary_i,targets)
+
+def binarize(cat_i,targets):
+    y_i=np.zeros((len(targets),2))
+    for j,target_j in enumerate(targets):
+        y_i[j][int(target_j==cat_i)]=1
+    return y_i
+
+def get_ens(ens_type):
+    if(ens_type=='One'):
+        return OneVsOne
+    return OneVsAll
--- train.py
+++ train.py
@@ -1,79 +1,10 @@
 import sys
-from pathlib import Path
-sys.path.append(str(Path('.').absolute().parent))
-import numpy as np
+#from pathlib import Path
+#sys.path.append(str(Path('.').absolute().parent))
 from sklearn.model_selection import RepeatedStratifiedKFold
 from skopt import BayesSearchCV
-from sklearn.base import BaseEstimator, ClassifierMixin
 import json
-import data,nn,learn,folds,utils
-
-class NeuralEnsemble(BaseEstimator, ClassifierMixin):
-    def __init__(self,n_hidden=250,n_epochs=200):
-        self.n_hidden=n_hidden
-        self.n_epochs=n_epochs
-
-    def fit(self,X,targets):
-        raise NotImplementedError
-
-    def predict_proba(self,X):
-        votes=[]
-        for extr_i,model_i in zip(self.extractors,self.models):
-            binary_i=extr_i.predict(X)
-            full_i=np.concatenate([X,binary_i],axis=1)
-            vote_i= model_i.predict_proba(full_i)
-            votes.append(vote_i)
-        votes=np.array(votes)
-        prob=np.sum(votes,axis=0)
-        return prob
-
-    def predict(self,X):
-        prob=self.predict_proba(X)
-        return np.argmax(prob,axis=1)
-
-    def save(self,out_path):
-        data.make_dir(out_path)
-        for i,extr_i in enumerate(self.extractors):
-            extr_i.save(f'{out_path}/{i}')
-
-class OneVsAll(NeuralEnsemble):
-    def fit(self,X,targets):
-        self.extractors=[]
-        self.models=[]
-        n_cats=max(targets)+1
-        for cat_i in range(n_cats):
-            y_i=binarize(cat_i,targets)
-            nn_params={'dims':X.shape[1],'n_cats':2}
-            nn_i=nn.SimpleNN(n_hidden=self.n_hidden)(nn_params)
-            nn_i.fit(X,y_i,
-                epochs=self.n_epochs,batch_size=32)
-            extractor_i= nn.get_extractor(nn_i)
-            self.extractors.append(extractor_i)
-            binary_i=extractor_i.predict(X)
-            clf_i=learn.get_clf('LR')
-            full_i=np.concatenate([X,binary_i],axis=1)
-            clf_i.fit(full_i,targets)
-            self.models.append(clf_i)
-
-class OneVsAll(NeuralEnsemble):
-    def fit(self,X,targets):
-        self.extractors=[]
-        self.models=[]
-        n_cats=max(targets)+1
-        pairs = list(combinations(range(n_cats),2))
-        for i,j in pairs:
-            selected=[k for k,y_k in enumerate(targets)
-                        if((y_k==i) or (y_k==j))]
-            y_s=[ int(targets[k]==i) for k in selected]    
-            y_s= tf.keras.utils.to_categorical(y_s, num_classes = 2)
-            X_s=np.array([ X[k,:] for k in selected])
-            nn_params={'dims':X.shape[1],'n_cats':2}
-            model_i=nn.SimpleNN(n_hidden=self.n_hidden)(nn_params)
-            model_i.fit(X_s,y_s,
-                epochs=self.n_epochs,batch_size=self.batch_size)
-            extractor_i= nn.get_extractor(model_i)
-            self.extractors.append(extractor_i)
-        return self.extractors
+import binary,data,nn,learn,folds,utils
 
 class BayesOptim(object):
     def __init__(self,clf_alg,search_spaces,n_split=5):
@@ -97,21 +28,24 @@ def find_hyperparams(train,params=None,ensemble_type=None,n_split=2):
         params={'n_hidden':[25,50,100,200],
                     'n_epochs':[100,200,300,500]}
     if(ensemble_type is None):
-        ensemble_type=NeuralEnsemble
+        ensemble_type=binary.get_ens('All')
     bayes_cf=BayesOptim(ensemble_type,params,n_split=n_split)
     train_tuple=train.as_dataset()[:2]
     best_params= bayes_cf(*train_tuple)
     return best_params
 
-def gen_data(in_path,out_path,n_iters=10,n_split=10,bayes=True):
+def gen_data(in_path,out_path,n_iters=10,n_split=10,bayes=True,
+                ens_type="all"):
     raw_data=data.read_data(in_path)
     data.make_dir(out_path)
+    NeuralEnsemble=binary.get_ens(ens_type)
     if(bayes):
-        hyperparams=find_hyperparams(raw_data)
+        hyperparams=find_hyperparams(raw_data,
+            ensemble_type=NeuralEnsemble,n_split=n_split)
     else:
-        hyperparams={'n_hidden':200,'n_epochs':200}
+        hyperparams={'n_hidden':250,'n_epochs':200}
     for i in range(n_iters):
-        out_ci=f'{out_path}/{i}'
+        out_i=f'{out_path}/{i}'
         data.make_dir(out_i)
         folds_i=folds.make_folds(raw_data,k_folds=n_split)
         splits_i=folds.get_splits(raw_data,folds_i)
@@ -121,12 +55,6 @@ def gen_data(in_path,out_path,n_iters=10,n_split=10,bayes=True):
             out_j=f'{out_i}/{j}'
             save_fold(ens_j,rename_j,out_j)
     
-def binarize(cat_i,targets):
-    y_i=np.zeros((len(targets),2))
-    for j,target_j in enumerate(targets):
-        y_i[j][int(target_j==cat_i)]=1
-    return y_i
-
 def save_fold(ens_j,rename_j,out_j):
     data.make_dir(out_j)
     ens_j.save(f'{out_j}/models')
@@ -148,12 +76,13 @@ if __name__ == "__main__":
     parser.add_argument("--n_split", type=int, default=10)
     parser.add_argument("--json", type=str, default='../uci/json/wine')
     parser.add_argument("--models", type=str, default='models')
-    parser.add_argument("--bayes", type=bool, default=True)
-    parser.add_argument("--multi", type=bool, default=True)
+    parser.add_argument("--bayes",action='store_true')
+    parser.add_argument("--multi", type=bool, default=False)
     args = parser.parse_args()
+#    raise Exception(args.bayes)
     if(args.multi):
         fun=multi_exp
     else:
         fun=gen_data
     fun(args.json,args.models,n_iters=args.n_iters,
-        n_split=args.n_split,bayes=args.bayes)
+        n_split=args.n_split,bayes=args.bayes,ens_type="One")
