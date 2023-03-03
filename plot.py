7fa734a6f335ee7232f994eefd777e9c88568f22
tjacek <tjacek@agh.edu.pl>
2023-03-03 22:02:58 +0100

    errorbar

--- plot.py
+++ plot.py
@@ -13,6 +13,19 @@ def find_best(in_path):
         best_result[data_i]=(row_k['mean_acc'],row_k['std_acc'])
     return best_result
 
+def box_plot(best_dict):
+    fig, ax = plt.subplots()
+    names=best_dict.keys()
+    for i,value_i in enumerate(best_dict.values()):
+        mean_i,std_i=value_i
+        plt.errorbar(i, mean_i, yerr = std_i,
+        	capsize=7,elinewidth=10,ecolor='b')
+    plt.xticks(range(len(names)),names)
+    plt.ylabel('Acc')
+    plt.xlabel('Dataset')   
+    plt.title('NECSCF')
+    plt.show()
+
 in_path='../uci_bayes/bayes/result.csv'
 best=find_best(in_path)
-print(best)
+box_plot(best)
