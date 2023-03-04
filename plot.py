28eb1586f0a26c984676b275fb90099a86dc7c83
tjacek <tjacek@agh.edu.pl>
2023-03-04 23:43:58 +0100

    best gen

--- plot.py
+++ plot.py
@@ -2,16 +2,19 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 
 def find_best(in_path):
-    result_df=pd.read_csv(in_path)
+    result_df=pd.read_csv(in_path)   
+    best_result={}
+    for data_i,row_i in best_gen(result_df):
+        best_result[data_i]=(row_i['mean_acc'],row_i['std_acc'])
+    return best_result
+
+def best_gen(result_df):
     dataset=result_df['dataset'].unique()
     ens_df=result_df[result_df['ens']=='NECSCF']
-    best_result={}
     for data_i in dataset:
         df_i=ens_df[ens_df['dataset']==data_i]
         k=df_i['mean_acc'].argmax()
-        row_k=df_i.iloc[k]
-        best_result[data_i]=(row_k['mean_acc'],row_k['std_acc'])
-    return best_result
+        yield data_i,df_i.iloc[k]
 
 def box_plot(best_dict):
     fig, ax = plt.subplots()
@@ -26,6 +29,7 @@ def box_plot(best_dict):
     plt.title('NECSCF')
     plt.show()
 
-in_path='../uci_bayes/bayes/result.csv'
-best=find_best(in_path)
-box_plot(best)
+if __name__ == "__main__":
+    in_path='../uci_bayes/bayes/result.csv'
+    best=find_best(in_path)
+    box_plot(best)
