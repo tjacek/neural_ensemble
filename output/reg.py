43ab8db6a6881d3f6f08f15aacdd6c9fbb5f1952
tjacek <tjacek@agh.edu.pl>
2023-03-19 21:56:36 +0100

    reg_eval

--- output/reg.py
+++ output/reg.py
@@ -4,10 +4,18 @@ from sklearn import linear_model
 from scipy import stats
 import statsmodels.api as sma
 from sklearn import preprocessing
-
 import plot
 
-def reg_frame(result,stats,clf=None):
+def reg_eval(result,stats):
+    df=reg_frame(result,stats)
+    features=['classes','samples','features','gini']
+    coff=linear_reg(df,features ,robust=False)
+    stat_sig= p_value(df)
+    print(','.join(features))
+    print(coff)
+    print(stat_sig)
+
+def reg_frame(result,stats):
     if(type(result)==str):
         result=pd.read_csv(result)
     if(type(stats)==str):
@@ -17,32 +25,31 @@ def reg_frame(result,stats,clf=None):
             index_j=(stats['dataset']==data_j)
             return stats[index_j][col_i].to_list()[0]
         result[col_i]=result['dataset'].apply(helper)
+    result['samples']=result['samples'].apply(lambda x:np.log(x))
     return result
 
-def linear_reg(df,robust=False):
+def linear_reg(df,features ,robust=False):
     if(robust):
         clf=linear_model.HuberRegressor()
     else:
         clf=linear_model.LinearRegression()
-    X=df[['classes','samples','features','gini']].to_numpy()
+    X=df[features].to_numpy()
     y=df['diff'].to_numpy()
+    X=preprocessing.normalize(X,axis=0)
     clf.fit(X,y)
     return clf.coef_/np.sum(np.abs(clf.coef_))
     
-
 def p_value(df):
     X=df[['classes','samples','features','gini']].to_numpy()
     y=df['diff'].to_numpy()
+    X=preprocessing.normalize(X,axis=0)
     est  = sma.OLS(y, X)
     output  = est.fit()
-    print(f'pvalue:{output.f_pvalue}')
-    print(output.summary2().tables[1]['P>|t|'])
+#    print(f'pvalue:{output.f_pvalue}')
+    return output.summary2().tables[1]['P>|t|']
 
 
 
 result_path='diff.csv'
 stats_path='stats.csv'
-df_reg=reg_frame(result_path,stats_path)
-coff=linear_reg(df_reg)
-print([f'{c:2f}' for c in coff])
-p_value(df_reg)
+reg_eval(result_path,stats_path)
