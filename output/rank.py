080efd3d14ce6bba099c77fd4dee30ec0e058815
tjacek <tjacek@agh.edu.pl>
2023-04-12 12:05:06 +0200

    rank of variants

--- output/rank.py
+++ output/rank.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def comput_rank(result_path):
     result_df=pd.read_csv(result_path)
+    no_ensemble=set(['pca-only','common'])
     all_ranks=[]
     for data_i in result_df['dataset'].unique():
         df_i= result_df[result_df['dataset']==data_i]
@@ -12,6 +13,8 @@ def comput_rank(result_path):
             var_j=row_j[['ens_type','clf_type','acc_mean']]
             var_j=var_j.to_numpy().tolist()
             var_j=[str(v) for v in var_j[0]]
+            if(var_j[0] in no_ensemble):
+                var_j[0]=f' {var_j[0]}'
             rank_j.append(','.join(var_j))
         rank_j.reverse()
         all_ranks.append('\n{}:\n{}'.format(data_i,'\n'.join(rank_j)))
