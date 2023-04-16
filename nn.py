79b3b1e78a132501128a57dc1cba6bcd0765f549
tjacek <tjacek@agh.edu.pl>
2023-04-16 17:24:28 +0200

    split nn

--- nn.py
+++ nn.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import tensorflow as tf
 import tensorflow.keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,BatchNormalization,Concatenate
@@ -105,12 +106,11 @@ class MulticlassNN(BaseEstimator, ClassifierMixin):
             y.append(y_i)
         X,y=np.array(X),np.array(y)
         self.fit(X,y,n_cats)
-        raise Exception(y.shape)
 
     def fit(self,X,targets,n_cats):
 #        n_cats=max(targets)+1
         n_hidden= int(self.ratio*X.shape[1])   
-        batch_size= conf.GLOBAL['batch_size']#int(conf.GLOBAL['batch_ratio']* X.shape[0])
+        batch_size=X.shape[0] #conf.GLOBAL['batch_size']
         params={'n_cats':n_cats,'dims':int(X.shape[1]/n_cats),
             'n_hidden':n_hidden}
         self.model=self.build_model(params)
@@ -126,17 +126,30 @@ class MulticlassNN(BaseEstimator, ClassifierMixin):
 
         input_dim=params['n_cats']*params['dims']
         input_layer=tensorflow.keras.layers.Input(shape=input_dim) #tensorflow.keras.layers.InputLayer(input_shape=input_dim)
+        
+        layer_splits=tf.split(input_layer, 
+            num_or_size_splits=params['n_cats'], axis=1)
         prob_layers=[]
-        for i in range(params['n_cats']):
-            input_i = input_layer[(i*dims):(i+1)*dims]
+        for i,input_i in enumerate(layer_splits):
             x_i=Dense(params['n_hidden'],activation='relu',name=f"hidden{i}",
                 kernel_regularizer=None)(input_i)
             x_i=BatchNormalization()(x_i)
             x_i=Dense(params['n_cats'], activation='softmax')(x_i)        
             prob_layers.append(x_i)
+
+#        prob_layers=[]
+#        for i in range(params['n_cats']):
+#            print(input_layer.shape)
+#            input_i = input_layer[(i*dims):(i+1)*dims]
+#            raise Exception(input_i.shape)
+#            x_i=Dense(params['n_hidden'],activation='relu',name=f"hidden{i}",
+#                kernel_regularizer=None)(input_i)
+#            x_i=BatchNormalization()(x_i)
+#            x_i=Dense(params['n_cats'], activation='softmax')(x_i)        
+#            prob_layers.append(x_i)
         concat_layer = Concatenate()(prob_layers)
         model= Model(inputs=input_layer, outputs=concat_layer)
         optim=optimizers.RMSprop(learning_rate=0.00001)
-        model.compile(loss='categorical_crossentropy',
+        model.compile(loss='mean_squared_error', #'categorical_crossentropy',
             optimizer=optim,metrics=['accuracy'])
         return model
