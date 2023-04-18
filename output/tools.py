ba7f6bfd465bcb9a005d0e33c9dbc04469dbc9bd
tjacek <tjacek@agh.edu.pl>
2023-04-18 23:36:32 +0200

    make_result

--- output/tools.py
+++ output/tools.py
@@ -1,7 +1,14 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path('.').absolute()))
+import numpy as np
+from collections import defaultdict
 import data,learn,utils
+
+#class ResultDict(object):
+#    def __init__(self,dataset,variants,clfs):
+#    def __init__(self, arg=[]):
+#        super(ResultDict, self).__init__(arg)
 
 def read_results(output):
     @utils.dir_fun(as_dict=True)
@@ -10,3 +17,16 @@ def read_results(output):
         return [ learn.read_result(path_j)
             for path_j in data.top_files(path_i)]
     return helper(output)
+
+def make_result(output):
+    raw_dict=read_results(output)
+    for data_i,dict_i in raw_dict.items():
+        new_dict_i= defaultdict(lambda:{})
+        for id_j,results_j in dict_i.items():
+            variant_j,clf_j=id_j.split('_')
+            new_dict_i[clf_j][variant_j]=mean_acc(results_j)
+        raw_dict[data_i]=new_dict_i
+    print(raw_dict)
+
+def mean_acc(results):
+    return np.mean([result_j.get_acc() for result_j in results])
