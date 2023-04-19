a9c423fbb9262c16577f6df82b5bcb2d793bfd8b
tjacek <tjacek@agh.edu.pl>
2023-04-19 23:26:24 +0200

    tools

--- output/reg.py
+++ output/reg.py
@@ -8,16 +8,15 @@ import tools
 
 def reg_exp(in_path,stats_path,base='common'):
     result_dict=tools.get_variant_results(in_path)
+    result_dict.transform()
     result_dict.to_diff(base)
     rows=result_dict.as_rows()
     df=pd.DataFrame(rows[1:],columns=rows[0])
     full_df= reg_frame(df,stats_path)
     variants=result_dict.variant_names()
-#    print(full_df['clf']=='RF')
     reg_eval(full_df,variants)
 
 def reg_eval(full_df,variants):
-#    df=reg_frame(result,stats)
     features=['classes','samples','features','gini']
     print( ','.join(['dataset','clf']+features))
     for var_i in variants:
@@ -26,10 +25,6 @@ def reg_eval(full_df,variants):
             coff=linear_reg(df_j,features,var_i,robust=False)
             line_ij=[var_i,clf_j]+[f'{c:.4f}' for c in coff]
             print(','.join(line_ij))
-#    stat_sig= p_value(df)
-#    print(','.join(features))
-#    print(coff)
-#    print(stat_sig)
 
 def reg_frame(result,stats):
     if(type(result)==str):
@@ -54,21 +49,26 @@ def linear_reg(df,indep_var,dep_var ,robust=False):
     X=preprocessing.normalize(X,axis=0)
     clf.fit(X,y)
     return clf.coef_/np.sum(np.abs(clf.coef_))
-    
-def p_value(df):
-    X=df[['classes','samples','features','gini']].to_numpy()
-    y=df['diff'].to_numpy()
-    X=preprocessing.normalize(X,axis=0)
-    est  = sma.OLS(y, X)
-    output  = est.fit()
+ 
+def p_value_exp(in_path):
+    result_dict=tools.get_variant_results(in_path)
+    for data_i,clf_j,dict_j in self.iter():
+        print(dict_i)
+#def p_value(df):
+#    X=df[['classes','samples','features','gini']].to_numpy()
+#    y=df['diff'].to_numpy()
+#    X=preprocessing.normalize(X,axis=0)
+#    est  = sma.OLS(y, X)
+#    output  = est.fit()
 #    print(f'pvalue:{output.f_pvalue}')
-    return output.summary2().tables[1]['P>|t|']
+#    return output.summary2().tables[1]['P>|t|']
 
 
 if __name__ == "__main__":
     in_path='../uci/ova_hyper/output'
     stats_path='../uci/stats.csv'
-    reg_exp(in_path,stats_path)
+#    reg_exp(in_path,stats_path)
+    p_value_exp(in_path)
 #    reg_exp(result_dict)
 #result_path='diff.csv'
 #stats_path='stats.csv'
--- output/tools.py
+++ output/tools.py
@@ -13,24 +13,33 @@ class VariantResults(dict):
         clf_i=list(self.values())[0]
         var_i=list(clf_i.values())[0]
         return var_i.keys()
-
-    def to_diff(self,base='common'):
+    
+    def iter(self):
         for data_i,dict_i in self.items():
             for clf_j,dict_j in dict_i.items():
-                base_value=dict_j[base]
-                del dict_j[base]
-                for key_i in dict_j:
-                    dict_j[key_i]-=base_value
+                yield data_i,clf_j,dict_j
+
+    def transform(self,fun=np.mean):
+        for data_i,clf_j,dict_j in self.iter():
+            for key_i,value_i in dict_j.items():
+                dict_j[key_i]=fun(value_i)
+
+    def to_diff(self,base='common'):
+        for data_i,clf_j,dict_j in self.iter():
+            base_value=dict_j[base]
+            del dict_j[base]
+            for key_i in dict_j:
+                dict_j[key_i]-=base_value
+                dict_j[key_i]/=base_value
     
     def as_rows(self):
         variants=self.variant_names()
         head=['dataset','clf']+list(variants)
         rows=[head]
-        for data_i,dict_i in self.items():
-            for clf_j,dict_j in dict_i.items():
-                line_i=[data_i,clf_j] 
-                line_i+=[dict_j[var] for var in variants]
-                rows.append(line_i)
+        for data_i,clf_j,dict_j in self.iter():    
+            line_i=[data_i,clf_j] 
+            line_i+=[dict_j[var] for var in variants]
+            rows.append(line_i)
         return rows
 
 def read_results(output):
@@ -47,9 +56,9 @@ def get_variant_results(output):
         new_dict_i= defaultdict(lambda:{})
         for id_j,results_j in dict_i.items():
             variant_j,clf_j=id_j.split('_')
-            new_dict_i[clf_j][variant_j]=mean_acc(results_j)
+            new_dict_i[clf_j][variant_j]=get_acc(results_j)
         raw_dict[data_i]=new_dict_i
     return VariantResults(raw_dict)
 
-def mean_acc(results):
-    return np.mean([result_j.get_acc() for result_j in results])
+def get_acc(results):
+    return [result_j.get_acc() for result_j in results]
