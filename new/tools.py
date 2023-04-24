a98d9a88037004f68f00a63a7c31ee2a06b41c61
tjacek <tjacek@agh.edu.pl>
2023-04-24 19:31:34 +0200

    tools module

---
+++ new/tools.py
@@ -0,0 +1,23 @@
+import os,warnings
+
+def silence_warnings():
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    import tensorflow as tf
+    import logging
+    logging.getLogger('tensorflow').setLevel(logging.ERROR)
+    tf.get_logger().setLevel('ERROR')
+    def warn(*args, **kwargs):
+        pass
+    warnings.warn = warn
+
+def make_dir(path):
+    if(not os.path.isdir(path)):
+        os.mkdir(path)
+
+def top_files(path):
+    if(type(path)==str):
+        paths=[ path+'/'+file_i for file_i in os.listdir(path)]
+    else:
+        paths=path
+    paths=sorted(paths)#,key=natural_keys)
+    return paths
