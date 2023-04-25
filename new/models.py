357711dba4205acbdd2fb6ddf5a45a4597668a56
tjacek <tjacek@agh.edu.pl>
2023-04-25 22:20:00 +0200

    read models

--- new/models.py
+++ new/models.py
@@ -6,6 +6,13 @@ class ModelIO(object):
         tools.make_dir(dir_path)
         self.dir_path=dir_path
 
+    def read(self):
+        for path_i in tools.top_files(self.dir_path):
+            clf_i=clfs.read_clf(path_i)     
+            train_i=np.load(f'{path_i}/train.npy')
+            test_i=np.load(f'{path_i}/test.npy')
+            yield clf_i,(train_i,test_i)
+
     def save(self,clf_i,i,split_i):
         out_i=f'{self.dir_path}/{i}'
         clfs.save_clf(clf_i,out_i)
