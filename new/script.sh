edd53a8cda433a4d73ccb2247968363da2d9a92e
tjacek <tjacek@agh.edu.pl>
2023-05-05 16:17:28 +0200

    fix script

--- new/script.sh
+++ new/script.sh
@@ -1,31 +1,27 @@
 data_path='uci'
-out_path='uci_out'
+out_path='../../cl/out'
 n_split=3
 n_repeats=3
 bayes_iter=2
-clfs='GPUClf_2_2,CPUClf_2'  #'all'
+clfs='GPU,CPU'  #'all'
 log_path="${out_path}/time.log"
-
-mkdir ${out_path}
-mkdir "${out_path}/hyper"
-mkdir "${out_path}/models"
-mkdir "${out_path}/results"
 
 for data_i in "$data_path"/*
 do
   echo "${data_i}"
   IFS="/" read -ra arr <<< "$data_i"
   name_i="${arr[1]}"
-  hyper_i="${out_path}/hyper/${name_i}"
-  model_i="${out_path}/models/${name_i}"
-  results_i="${out_path}/results/${name_i}"
+  hyper_i="${out_path}/${name_i}/hyper.txt"
+  model_i="${out_path}/${name_i}/models"
+  results_i="${out_path}/${name_i}/results"
+  pvalue_i="${out_path}/${name_i}/pvalue.txt"
 
-  python3 hyper.py --data "${data_i}" --hyper "${hyper_i}" \
-        --n_split ${n_split} --n_iter ${bayes_iter} --clfs ${clfs} \
-        --log_path ${log_path}
+#  python3 hyper.py --data "${data_i}" --hyper "${hyper_i}" \
+#        --n_split ${n_split} --n_iter ${bayes_iter} --clfs ${clfs} \
+#        --log_path ${log_path}
   python3 train.py --data "${data_i}" --hyper "${hyper_i}" \
-        --out "${model_i}" --ens best --n_splits "${n_split}" \
+        --out "${model_i}" --ens 'GPU,CPU' --n_splits "${n_split}" \
         --n_repeats "${n_repeats}" --log_path ${log_path}
   python3 eval.py --data ${data_i} --models ${model_i} \
-        --results ${results_i} --log_path ${log_path}
+        --results ${results_i} --p_value ${pvalue_i} --log_path ${log_path}
 done
