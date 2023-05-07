a9174d4bd0a731aa5c932682261425e9d5fbb326
tjacek <tjacek@agh.edu.pl>
2023-05-07 13:42:26 +0200

    metrics as arg

--- new/summary.py
+++ new/summary.py
@@ -2,13 +2,13 @@ import argparse
 import pandas as pd
 import tools
 
-def make_summary(dir_path,out_path):
+def make_summary(dir_path,out_path,metric='acc_mean'):
     paths=tools.get_dirs(dir_path)
     for path_i in paths:
         name_i=path_i.split('/')[-1]
         result_i=f'{path_i}/results'
         df=pd.read_csv(result_i) 
-        df=df.sort_values(by="acc_mean",ascending=False)
+        df=df.sort_values(by=metric,ascending=False)
         df_pvalue=pd.read_csv(f'{path_i}/pvalue.txt') 
         with open(out_path,"a") as f:
             f.write(f'{name_i}\n')
@@ -27,11 +27,12 @@ def short_summary(dir_path,out_path):
         sig_i=(df_pvalue[df_pvalue['sig']==True])
         for j, row_j in sig_i.iterrows():
             ens_j,clf_j=row_j['ens'],row_j['clf']
+#            if((clf_j in ens_j) ):#or ('TF' in ens_j)):
+
             ens_acc= df[df['clf']==ens_j]['acc_mean']
             clf_acc= df[df['clf']==clf_j]['acc_mean']
             diff= (float(ens_acc)-float(clf_acc))
             line_j=[name_i,ens_j,clf_j,(diff>0),diff ]
-            print(line_j)
             lines.append(line_j)
     df= pd.DataFrame(lines,columns=cols)
     print(df)
@@ -39,6 +40,8 @@ def short_summary(dir_path,out_path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", type=str, default='../../cl/out')
+    parser.add_argument("--metric", type=str, default='balanced_acc_mean')
+
     parser.add_argument("--out", type=str, default='../../cl/out/summary.txt')
     parser.add_argument("--short",action='store_true')
 
@@ -46,4 +49,4 @@ if __name__ == '__main__':
     if(args.short):
         short_summary(args.dir,args.out)
     else:
-        make_summary(args.dir,args.out)
+        make_summary(args.dir,args.out,args.metric)
