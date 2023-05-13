9c1fdf5efdb0b2d29a7d243e3ab8b5170f5258ff
tjacek <tjacek@agh.edu.pl>
2023-05-13 13:18:11 +0200

    read predictions

--- new/eval.py
+++ new/eval.py
@@ -1,88 +1,20 @@
 import tools
 tools.silence_warnings()
 import argparse
-import numpy as np
-from time import time
-from collections import defaultdict
-import json
-import clfs,models,variants,learn
+import pred
 
-class AllPreds(object):
-    def __init__(self):
-        self.pred={}  
-        self.true={}
-
-    def compute_metric(self,metric_type='acc'):
-        metric=learn.get_metric(metric_type)
-        metrict_dict=defaultdict(lambda :[])
-        for i,pred_dict in self.pred.items():
-            true_i=self.true[i]
-            for name_j,pred_j in pred_dict.items():
-                metrict_dict[name_j].append(metric(true_i,pred_j) )
-        return metrict_dict
-
-    def save(self,out_path):
-        raw_dict={'pred':self.pred,'true':self.true}
-        with open(out_path, 'w') as f:
-            json.dump(raw_dict, f,cls=NumpyEncoder)
-
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
-
-def single_exp(data_path,model_path,result_path,p_value):
-    X,y=tools.get_dataset(data_path)
-    pred_dict= get_pred_dict(X,y,model_path)
-    metric_dict=pred_dict.compute_metric()
-    stats(metric_dict)
-    pred_dict.save(result_path)
-
-def get_pred_dict(X,y,model_path):
-    clf_types=['RF','SVC']
-    variant_types=['NECSCF','common']
-    pred_dict=AllPreds() 
-    model_reader=models.ManyClfs(model_path)
-    for i,model_dict_i,train_i,test_i in model_reader.split(X,y):
-        pred_dict.true[i]=test_i[1]
-        pred_dict.pred[i]={}
-        for name_j,pred_j in pred_iter(model_dict_i,
-                                       train_i,test_i,
-                                       clf_types,variant_types):
-            pred_dict.pred[i][name_j]=pred_j
-    return pred_dict
-
-def pred_iter(model_dict_i,train_i,test_i,
-        clf_types,variant_types):
-    for name_j,nn_j in model_dict_i.items(): 
-        if(clfs.is_cpu(nn_j)):
-            ens_j=variants.make_ensemble(nn_j,train_i,test_i)
-            for variant in  variant_types:
-                for clf in clf_types:
-                    pred_j=ens_j(clf,variant)
-                    yield f'{variant}({clf})',pred_j
-        else:
-            pred_j=nn_j.predict(test_i[0])
-            yield 'NECSCF(NN-TF)',pred_j
-
-def stats(metric_dict):
-    for name_i,metric_i in metric_dict.items():
-        print(f'{name_i},{np.mean(metric_i):.4f},{np.std(metric_i):.4f}')
+def single_exp(pred_path,result_path,pvalue_path):
+    pred_dict= pred.read_preds(pred_path)
+    print(pred_dict)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, default='uci/vehicle')
-    parser.add_argument("--models", type=str, default='vehicle/models')
-    parser.add_argument("--results", type=str, default='vehicle/results2')
+    parser.add_argument("--pred", type=str, default='vehicle/results2')
+    parser.add_argument("--results", type=str, default='vehicle/p_value')
     parser.add_argument("--p_value", type=str, default='vehicle/p_value')
-    parser.add_argument("--log_path", type=str, default='vehicle/log.time')
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args=parse_args()
-    tools.start_log(args.log_path)
-    start=time()
-    single_exp(args.data,args.models,args.results,args.p_value)
-    tools.log_time(f'EVAL:{args.data}',start) 
+    single_exp(args.pred,args.results,args.p_value)
--- new/single.sh
+++ new/single.sh
@@ -1,33 +1,39 @@
-name='wine-quality-red' 
+name='lymphography' 
 data="uci/${name}"
+#name="../../cl/out/${name}"
 hyper="${name}/hyper.txt"
 models="${name}/models"
 log="${name}/log.time"
 pvalue="${name}/pvalue.txt"
+pred="${name}/pred"
 results="${name}/results"
-n_split=3
-n_repeats=3
+acc_path="${name}/acc.txt"
+n_split=10
+n_repeats=10
 bayes_iter=5
 
-hyper_cmd=false
+hyper_cmd=true
 
 train_model () {
-python3 train.py --data ${data} --hyper ${hyper} \
-    --ens $1 --out "${models}_$1" --log_path ${log} \
-    --n_split ${n_split} --n_repeats ${n_repeats} 
+  python3 train.py --data ${data} --hyper ${hyper} \
+    --ens $1 --out "${models}" --log_path ${log} \
+    --n_split ${n_split} --n_repeats ${n_repeats} \
+    --acc_path ${acc_path}
 }
-
 
 mkdir ${name}
 
 #if [ $hyper_cmd == true ]; then {
-#  python3 hyper_keras.py --data ${data} --hyper ${hyper} \
+#  python3 hyper.py --data ${data} --hyper ${hyper} \
 #    --n_split ${n_split} --n_iter ${bayes_iter} \
 #    --clfs 'all' --log_path ${log}
 #}
+#fi
 
-train_model "GPU,CPU"
-#train_model "CPU"
+#train_model "GPU,CPU"
 
-#python3 eval.py --data ${data} --models ${models} \
-#     --results ${results} --log_path  ${log} --p_value ${pvalue}
+#python3 pred.py --data ${data} --models ${models} \
+#     --out ${pred} --log_path ${log} 
+
+python3 eval.py --pred ${pred} --results ${results} \
+  --p_value ${pvalue}
