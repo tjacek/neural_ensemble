bec2a6f74db9bdb9b358b8b6475ec31d6a983b31
tjacek <tjacek@student.agh.edu.pl>
2023-05-18 22:23:49 +0200

    script

--- new/script.sh
+++ new/script.sh
@@ -1,42 +1,49 @@
 data_path='../../uci'
-out_path='../../acc_min'
+out_path='../../prec_prune'
 n_split=10
 n_repeats=10
 bayes_iter=20
 clfs="all" #'GPU,CPU'  
 log_path="${out_path}/time.log"
+result_dir="high"
 
 mkdir ${out_path}
 
 for data_i in "$data_path"/*
 do
-  echo "${data_i}"
+#  echo "${data_i}"
   IFS="/" read -ra arr <<< "$data_i"
   name_i="${arr[-1]}"
   dir_path="${out_path}/${name_i}"
-  echo ${dir_path}
+#  echo ${dir_path}
   mkdir ${dir_path}
 
-  hyper_i="${out_path}/${name_i}/hyper.txt"
-  model_i="${out_path}/${name_i}/models"
-  results_i="${out_path}/${name_i}/results"
-  pvalue_i="${out_path}/${name_i}/pvalue.txt"
-  pred_i="${out_path}/${name_i}/pred"
-  acc_i="${out_path}/${name_i}/acc.txt"
+  hyper_i="${dir_path}/hyper.txt"
+  model_i="${dir_path}/models"
+  acc_i="${dir_path}/acc.txt"
 
 #  python3 hyper_binary.py --data "${data_i}" --hyper "${hyper_i}" \
 #    --n_split ${n_split} --n_iter ${bayes_iter} --clfs ${clfs} --log_path ${log_path}
 
-#  python3 train.py --data "${data_i}" --hyper "${hyper_i}" \
-#        --out "${model_i}" --ens 'CPU' --n_splits "${n_split}" \
-#        --n_repeats "${n_repeats}" --log_path ${log_path} --acc_path "${acc_i}"
+  python3 train.py --data "${data_i}" --hyper "${hyper_i}" \
+        --out "${model_i}" --ens 'CPU' --n_splits "${n_split}" \
+        --n_repeats "${n_repeats}" --log_path ${log_path} --acc_path "${acc_i}"
+ 
+  result_dir_i="${dir_path}/${result_dir}"
+  echo "${result_dir_i}"
+  mkdir ${result_dir_i}
+  results_i="${result_dir_i}/results"
+  pvalue_i="${result_dir_i}/pvalue.txt"
+  pred_i="${result_dir_i}/pred"
 
-#  python3 pred.py --data ${data_i} --models ${model_i} \
+#  python3 prune.py --data ${data_i} --models ${model_i} \
 #     --out ${pred_i} --log_path ${log_path} 
 
-#  python3 eval.py --pred ${pred_i} --results ${results_i} \
-#     --p_value ${pvalue_i}
+ # python3 eval.py --pred ${pred_i} --results ${results_i} \
+ #    --p_value ${pvalue_i}
 
-  python3 summary.py --dir ${out_path}  --metric 'acc_mean' --out "${out_path}/summary.txt"
+  
 #        --results ${results_i} --p_value ${pvalue_i} --log_path ${log_path}
 done
+
+#python3 summary.py --dir ${out_path}  --metric 'acc_mean' --out "${out_path}/summary.txt"
