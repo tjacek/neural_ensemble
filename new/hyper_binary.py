4936c48e0a66b929684aaa2b1a13f79706cb3c81
tjacek <tjacek@agh.edu.pl>
2023-05-30 23:09:45 +0200

    hyper_binary

--- new/hyper_binary.py
+++ new/hyper_binary.py
@@ -1,12 +1,13 @@
 import tools
 tools.silence_warnings()
 from time import time
+import numpy as np
 from skopt.space import Real#, Categorical, Integer
 import clfs,hyper
 
 def single_exp(data_path,hyper_path,n_split,n_iter,ens_types):
     X,y=tools.get_dataset(data_path)
-    bayes_optim=hyper.BayesOptim(scoring=binary_minacc,n_split=n_split,
+    bayes_optim=hyper.BayesOptim(scoring=mean_acc,n_split=n_split,
         n_repeats=1,n_iter=n_iter)	
     with open(hyper_path,"a") as f:
         f.write(f'data:{data_path},{bayes_optim.get_setting()}\n')
@@ -14,7 +15,7 @@ def single_exp(data_path,hyper_path,n_split,n_iter,ens_types):
 #        if(clfs.is_cpu(ens_type_i)):
         print(ens_type_i)
         ens_i= clfs.get_ens(ens_type_i)
-        search_i={hyper_i: Real(0.25, 5.0, prior='log-uniform') 
+        search_i={hyper_i: Real(0.25, 5.0, prior='uniform') #'log-uniform') 
             for hyper_i in clfs.params_names(ens_i)}
         param_dict,best_score=bayes_optim(X,y,ens_i,search_i)
         param_dict={ key_i:hyper.round_hype(hyper_i) 
@@ -24,10 +25,16 @@ def single_exp(data_path,hyper_path,n_split,n_iter,ens_types):
         with open(hyper_path,"a") as f:
             f.write(f'{ens_name},{str(param_dict)},{best_score}\n')
 
-def binary_minacc(estimator, X, y):
+def min_acc(estimator, X, y):
     acc_desc=estimator.catch
     acc_min= min(list(acc_desc.values()))  
     return acc_min
+
+def mean_acc(estimator, X, y):
+    acc_desc=estimator.catch
+    acc_mean= np.mean(list(acc_desc.values()))  
+    return acc_mean
+
 
 if __name__ == '__main__':
     args=hyper.parse_args()
