6724e943fc7f7a6d894174cd6f5118e5a15abc0a
tjacek <tjacek@agh.edu.pl>
2023-06-23 21:16:24 +0200

    eval model

--- train.py
+++ train.py
@@ -1,14 +1,27 @@
 import tools
 tools.silence_warnings()
 import argparse
+import numpy as np
+import tensorflow as tf 
+from sklearn.metrics import accuracy_score#,f1_score
+
 import data,deep
 
 def single_exp(data_path,hyper_path):
     X,y=data.get_dataset(data_path)
     hyper_params=parse_hyper(hyper_path)
     dataset_params=data.get_dataset_params(X,y)
-    model=deep.simple_nn(dataset_params,hyper_params)
-    print(dir(model))
+    splits=data.gen_splits(X,y,n_splits=10,n_repeats=1)
+    acc=[]
+    for (X_train,y_train),(X_test,y_test) in splits():
+        model=deep.simple_nn(dataset_params,hyper_params)
+        y_train = tf.keras.utils.to_categorical(y_train, 
+        	                num_classes = dataset_params['n_cats'])
+        model.fit(X_train,y_train)
+        y_pred= model.predict(X_test)
+        y_pred=np.argmax(y_pred,axis=1)
+        acc.append(accuracy_score(y_test,y_pred))
+    print(acc)
 
 def parse_hyper(hyper_path):
     with open(hyper_path) as f:
