b307fcc38a7b6cbc26551d5d7e2c39e1ebdd999d
tjacek <tjacek@agh.edu.pl>
2023-06-26 21:14:00 +0200

    save pred

--- pred.py
+++ pred.py
@@ -4,18 +4,26 @@ import argparse
 import numpy as np
 import tensorflow as tf 
 from keras import callbacks
+from collections import defaultdict
 import json
 import data,deep
 
 def single_exp(data_path,model_path,out_path):
     X,y=data.get_dataset(data_path)
+    pred_dict=defaultdict(lambda:[])
     for name_i,model_i,split_i in get_model_paths(model_path):
         test_X, test_y=split_i.get_test(X,y)
         y_pred=model_i.predict(test_X)
         y_pred=np.argmax(y_pred,axis=1)
-        print(list(zip(test_y,y_pred)))
-
-
+        pred_dict[name_i].append((y_pred,test_y))
+    tools.make_dir(out_path)
+    for name_i,pred_i in pred_dict.items():
+        with open(f'{out_path}/{name_i}', 'wb') as f:
+            all_pred=[ (test_i.tolist(),pred_i.tolist()) 
+                        for test_i,pred_i in pred_i]
+            json_str = json.dumps(all_pred, default=str)         
+            json_bytes = json_str.encode('utf-8') 
+            f.write(json_bytes)
 
 def get_model_paths(model_path):
     for path_i in tools.get_dirs(model_path):
@@ -28,27 +36,6 @@ def get_model_paths(model_path):
             train_ind=np.load(f'{model_j}/train.npy')
             split_j=data.DataSplit(train_ind,test_ind)
             yield name_i,nn_j,split_j
-
-def make_pred(make_model,out_path,params,splits):
-    hyper_params,dataset_params=params
-    earlystopping = callbacks.EarlyStopping(monitor='accuracy',#params['metric'],
-                mode="max", patience=5,restore_best_weights=True)
-    all_pred=[]
-    for (X_train,y_train),(X_test,y_test) in splits():
-        model=make_model(dataset_params,hyper_params)
-        y_train = tf.keras.utils.to_categorical(y_train, 
-        	                num_classes = dataset_params['n_cats'])
-        model.fit(X_train,y_train,epochs=150,callbacks=earlystopping)
-        y_pred= model.predict(X_test)
-        y_pred=np.argmax(y_pred,axis=1)
-        all_pred.append((y_test,y_pred))
-    with open(out_path, 'wb') as f:
-        all_pred=[ (test_i.tolist(),pred_i.tolist()) 
-            for test_i,pred_i in all_pred]
-        json_str = json.dumps(all_pred, default=str)         
-        json_bytes = json_str.encode('utf-8') 
-        f.write(json_bytes)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
