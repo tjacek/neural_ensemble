244f1ef2f1916319245482876330038553185b73
tjacek <tjacek@agh.edu.pl>
2023-06-29 02:43:21 +0200

    metric_dict

--- eval.py
+++ eval.py
@@ -6,23 +6,33 @@ from sklearn.metrics import accuracy_score,balanced_accuracy_score#,f1_score
 import json
 
 def single_exp(pred_path,out_path):
-    metrics=[accuracy_score,balanced_accuracy_score]
+    acc_dict=metric_dict(accuracy_score,pred_path)
     stats=[np.mean,np.std]
+    for id_i,metric_i in acc_dict.items():
+        line_i=[id_i]+[f'{stat_j(metric_i):.4}' 
+                        for stat_j in stats]
+        print(','.join(line_i))
+
+def metric_dict(metric_i,pred_path):
+    metric_dict={}
     for path_i in tools.top_files(pred_path):
         all_pred=read_pred(path_i)
-        line_i=[f'{path_i}']
-        for metric_j in metrics:
-            acc=[ metric_j(test_i,pred_i) 
-                    for test_i,pred_i in all_pred]
-            for stat_k in stats:
-                line_i.append(f'{stat_k(acc):.4}')
-        print(','.join(line_i))
+        id_i=get_id(path_i)
+        line_i=[get_id(path_i)]
+        acc=[ metric_i(test_i,pred_i) 
+                for test_i,pred_i in all_pred]
+        metric_dict[id_i]=acc
+    return metric_dict
 
 def read_pred(path_i):
     with open(path_i, 'r') as f:        
         json_bytes = f.read()                      
         return json.loads(json_bytes)
-               
+
+def get_id(path_i):
+    raw=path_i.split('/')
+    return f'{raw[-2]},{raw[-1]}'
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--pred", type=str, default='10_10/pred')
