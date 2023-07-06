888681f499b8fd6c70f74f7e5f544ce7bab6e2f8
tjacek <tjacek@agh.edu.pl>
2023-07-06 15:35:43 +0200

    borda frame

--- output/rank.py
+++ output/rank.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 
-def comput_rank(result_path,metric_col='mean'):
+def compute_rank(result_path,metric_col='mean'):
     result_df=pd.read_csv(result_path)
     ranks=defaultdict(lambda :[])
     for data_i in result_df['dataset'].unique():
@@ -10,16 +10,19 @@ def comput_rank(result_path,metric_col='mean'):
         df_i= df_i.sort_values(by=metric_col,ascending=False)
         for i,clf_i in enumerate(df_i['clf']):
             ranks[clf_i].append(i+1)
+    print(borda_frame(ranks))
+
+def borda_frame(ranks):
     lines=[]
     for data_i,rank_i in ranks.items():
         lines.append([data_i,borda_count(rank_i)])
     borda_df = pd.DataFrame(lines,columns=['dataset','borda'])
     borda_df= borda_df.sort_values(by='borda')
-    print(borda_df)
+    return borda_df
 
 def borda_count(ranks):
     return np.sum([(1/r) for r in ranks])
 
 if __name__ == "__main__":
     result_path= 'result.csv'
-    comput_rank(result_path)
+    compute_rank(result_path)
--- output/sig.py
+++ output/sig.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import stats
+from collections import defaultdict
 import tools
+import rank
 
 def sig_group(in_path):
     acc_dict= tools.metric_dict('acc',in_path)
@@ -10,12 +12,15 @@ def sig_group(in_path):
         r=stats.ttest_ind(acc_dict[x], 
                 acc_dict[y], equal_var=False)
         return r[1]#round(r[1],4)
+    all_groups=[]
+    counter=1
     while(len(mean_dict)>0):
         group,worse=select_sig(mean_dict,helper)
         mean_dict={name_i:mean_dict[name_i] 
             for name_i in worse}
-        print(group)
-#        print(worse)
+        all_groups.append((counter,group))
+        counter+=1
+    return all_groups
 
 def select_sig(mean_dict,helper):
     names,score = zip(*mean_dict.items())
@@ -25,16 +30,19 @@ def select_sig(mean_dict,helper):
     for name_i in names:
         if(name_i==best):
         	pass
-        elif(helper(best,name_i)<0.05 ):
+        elif(helper(best,name_i)>0.05 ):
         	group.append(name_i)
         else:
         	worse.append(name_i)
     return group,worse
 
-
-
 if __name__ == "__main__":
     in_path='pred'
-    for path_i in tools.top_files(in_path):
-        sig_group(path_i)
-#    print(acc_dict)
+    sig=[ sig_group(path_i) for path_i in tools.top_files(in_path)]   
+    rank_dict=defaultdict(lambda:[])
+    for data_i in sig:
+    	for rank_j,algs_j in data_i:
+    		for id_k in algs_j:
+    		    rank_dict[id_k.split(',')[-1]].append(rank_j)
+    df=rank.borda_frame(rank_dict)
+    print(df)
