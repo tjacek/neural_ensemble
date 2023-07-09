2f3c846dd3c79fa0a7be1c97f29e5008990b18b9
tjacek <tjacek@agh.edu.pl>
2023-07-09 13:49:15 +0200

    format df

--- output/format.py
+++ output/format.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
+import re
 import pandas as pd
-
 
 result_path="result.csv"
 result_df=pd.read_csv(result_path)
@@ -23,11 +23,31 @@ for type_j in types:
     df_i.drop('type', inplace=True, axis=1)
     df_dict[type_j]=df_i
 
-nn_df=df_dict['NN']
+#nn_df=df_dict['NN']
 
 #df_dict['NN']=nn_df[ (nn_df['clf']!='binary_ens-0.25') 
 #                & (nn_df['clf']!='binary_ens-0.5')]
 
 #print(df_dict['NN'].to_latex())
-print(df_dict['SVC'].to_latex())
+
+clf='RF'
+
+def get_alpha(raw):
+    digits=re.findall(r'\d+',raw)
+    if(len(digits)>0):
+        return f'0.{digits[1]}'
+    return '-'
+
+def get_cs(raw):
+    cs= raw.split('_')[0]
+    if(cs==clf):
+        return '-'
+    return cs
+
+df=df_dict[clf]
+df['alpha']=df['clf'].apply(get_alpha)
+df['cs']=df['clf'].apply(get_cs)
+df['clf']=df['clf'].apply(lambda x:clf)
+df=df[['dataset','cs','alpha','clf','mean','std']]
+print(df.to_latex())
 
