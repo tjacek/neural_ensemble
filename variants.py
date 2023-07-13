3ff1d0b438232989e61eb44eb0c0633622eeb801
tjacek <tjacek@agh.edu.pl>
2023-07-13 23:55:13 +0200

    variants

--- variants.py
+++ variants.py
@@ -1,21 +1,29 @@
-from collections import defaultdict
-import data,pred,tools
+#from collections import defaultdict
+import numpy as np
+import learn#data,pred,tools
 
-def single_exp(data_path,model_path,out_path):
-    X,y=data.get_dataset(data_path)
-    pred_dict=defaultdict(lambda:[])
-    for name_i,model_i,split_i in pred.get_model_paths(model_path):
-        if('binary' in name_i):
-            train,test=split_i.get_dataset(X,y)
-            cs_train=model_i.extract(train.X)
-            cs_test=model_i.extract(test.X)
-            pred_k=pred.necscf(train,test,cs_train,cs_test,"LR")
-            pred_dict[f'{name_i}(LR)'].append(pred_k)
-    tools.make_dir(out_path)
-    for name_i,pred_i in pred_dict.items():
-        pred.save_pred(f'{out_path}/{name_i}',pred_i)
+class NoEnsemble(object):
+    def __init__(self,clfs):
+        self.clfs=clfs
 
-if __name__ == '__main__':
-#    if(args.dir>0):
-    single_exp=tools.dir_fun(3)(single_exp)
-    single_exp('../uci',"../10_10/models","pred")
+    def __call__(self,train,test):
+        for clf_type_i in self.clfs:
+            clf_i=learn.get_clf(clf_type_i)
+            clf_i.fit(train.X,train.y)
+            pred_i=clf_i.predict(test.X)
+            yield clf_type_i,pred_i
+
+def necscf(train,test,clf_type,votes=False):
+    votes=[]
+    for cs_train_i,cs_test_i in zip(train.cs,test.cs):
+        full_train_i=np.concatenate([train.X,cs_train_i],axis=1)       
+        clf_i=learn.get_clf(clf_type)
+        clf_i.fit(full_train_i,train.y)
+        full_test_i=np.concatenate([test.X,cs_test_i],axis=1)
+        y_pred=clf_i.predict_proba(full_test_i)
+        votes.append(y_pred)
+    if(votes):
+        return votes
+    votes=np.array(votes)
+    votes=np.sum(votes,axis=0)
+    return np.argmax(votes,axis=1)
