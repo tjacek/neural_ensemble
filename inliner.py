197929ce6a3b83b5a864339d39d773decd492406
tjacek <tjacek@agh.edu.pl>
2023-07-13 18:38:43 +0200

    inliner variant

--- inliner.py
+++ inliner.py
@@ -10,44 +10,52 @@ import data,pred
 
 @tools.log_time(task='INLINER')
 def single_exp(data_path,model_path,out_path):
-    clfs=['RF','SVC']
+    clfs=['RF','SVC']  
     X,y=data.get_dataset(data_path)
     pred_dict=defaultdict(lambda:[])
+    inliner_voting=InlinerVariant(clfs,
+                                  thres=3,
+                                  n_neighbors=3)
     for name_i,exp_i in pred.get_exps(model_path):
         if(exp_i.is_ens()):
             train_i,test_i=exp_i.get_features(X,y)
-            for clf_k,pred_k in inliner_voting(train_i,test_i,clfs):
-                id_k=f'{name_i}-{clf_k}-inliner'
+            for clf_k,pred_k in inliner_voting(train_i,test_i):
+                id_k=f'{name_i}-{clf_k}' #'{clf_k}-inliner'
                 print(id_k)
                 pred_dict[id_k].append((pred_k,test_i.y))
     tools.make_dir(out_path)
     for name_i,pred_i in pred_dict.items():
         pred.save_pred(f'{out_path}/{name_i}',pred_i)
 
-def inliner_voting(train_i,test_i,clfs,thres=3):
-    neigh = KNeighborsClassifier(n_neighbors=3)
-    y_near=[]
-    for j,cs_j in enumerate(train_i.cs):
-        neigh.fit(cs_j,train_i.y)
-        y_near.append(neigh.predict(test_i.cs[j]))
-    y_near=list(zip(*y_near))
-    n_samples=test_i.y.shape[0]
-    for clf_j in clfs:
-        votes=pred.necscf(train_i,test_i,clf_j,True)
-        votes=[[ vote_t[k,:] for vote_t in votes]
+class InlinerVariant(object):
+    def __init__(self,clfs,thres=3,n_neighbors=3):
+        self.clfs=clfs
+        self.thres=thres
+        self.n_neighbors=n_neighbors
+
+    def __call__(self,train_i,test_i):
+        neigh = KNeighborsClassifier(n_neighbors=3)
+        y_near=[]
+        for j,cs_j in enumerate(train_i.cs):
+            neigh.fit(cs_j,train_i.y)
+            y_near.append(neigh.predict(test_i.cs[j]))
+        y_near=list(zip(*y_near))
+        n_samples=test_i.y.shape[0]
+        for clf_j in clfs:
+            votes=pred.necscf(train_i,test_i,clf_j,True)
+            votes=[[ vote_t[k,:] for vote_t in votes]
                     for k in range(n_samples)]
-        y_pred=[]
-        for k,vote_k in enumerate(votes):
-#            print(y_near[k])
-            pred_k=np.argmax(vote_k,1)
-            s_vote=[ vote_k[t] 
+            y_pred=[]
+            for k,vote_k in enumerate(votes):
+                pred_k=np.argmax(vote_k,1)
+                s_vote=[ vote_k[t] 
                      for t,(pred,near) in enumerate(zip(pred_k,y_near[k]))
                          if(pred==near)]
-            if(len(s_vote)<thres):
-                s_vote=vote_k
-            s_vote=np.sum(s_vote,axis=0)
-            y_pred.append(np.argmax(s_vote,axis=0))
-        yield clf_j,np.array(y_pred)
+                if(len(s_vote)<thres):
+                    s_vote=vote_k
+                s_vote=np.sum(s_vote,axis=0)
+                y_pred.append(np.argmax(s_vote,axis=0))
+            yield f'{clf_j}-inliner',np.array(y_pred)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
