d2dc093c6e1aee5c47ba002c731c836fffa69a4e
tjacek <tjacek@agh.edu.pl>
2023-07-19 13:43:02 +0200

    alpha_optim

--- scikit.py
+++ scikit.py
@@ -5,20 +5,20 @@ from sklearn.model_selection import RepeatedStratifiedKFold
 from skopt import BayesSearchCV
 from skopt.space import Real, Categorical, Integer
 from sklearn.base import BaseEstimator, ClassifierMixin
-import data,deep,learn
+import data,deep,learn,train
 
 class ScikitAdapter(BaseEstimator, ClassifierMixin):
-    def __init__(self, alpha, hyper):
+    def __init__(self, alpha=0.5, hyper=None):
         self.alpha = alpha
         self.hyper = hyper
         self.neural_ensemble=None 
         self.clfs=[]
 
     def fit(self,X,targets):
-        ens_factory=deep.get_ensemble('weighted')
-        params=get_dataset_params(X) 
+        ens_factory=deep.get_ensemble(('weighted',self.alpha))
+        params=data.get_dataset_params(X,targets) 
         self.neural_ensemble=ens_factory(params,self.hyper)
-        self.neural_ensemble.fit(self,X,targets)
+        self.neural_ensemble.fit(X,targets)
         full=self.neural_ensemble.get_full(X) #.extract(X)
         for full_i in full:
             clf_i=learn.get_clf('RF')
@@ -35,22 +35,31 @@ class ScikitAdapter(BaseEstimator, ClassifierMixin):
         prob=self.predict_proba(X)
         return np.argmax(prob,axis=1)
 
-def alpha_optim(hyper,n_split,n_repeats, n_iter):
-    search_space={'alpha': Real(0.1, 0.9, prior='uniform'),
-                  'hyper':[hyper] }
+def alpha_optim(data_path,hyper_path,n_split,n_repeats, n_iter):
+    X,y=data.get_dataset(data_path)
+    hyper_dict=train.parse_hyper(hyper_path)
+    search_spaces={'alpha': Real(0.1, 0.9, prior='uniform')}
+#                   'hyper':Categorical([hyper_dict])}
     cv_gen=RepeatedStratifiedKFold(n_splits=n_split, 
-                n_repeats=n_repeats, random_state=1)
-    search = BayesSearchCV(estimator=clf,verbose=0,n_iter=self.n_iter,
-                search_spaces=search_spaces,n_jobs=1,cv=cv_gen,
-                scoring=self.scoring)
+                                   n_repeats=n_repeats, 
+                                   random_state=1)
+    search = BayesSearchCV(estimator=ScikitAdapter(0.5,hyper_dict),
+                           n_iter=n_iter,
+                           search_spaces=search_spaces,
+                           cv=cv_gen,
+                           scoring='accuracy',
+                           verbose=0,
+                           n_jobs=1,)
+    search.fit(X,y)#,callback=callback) 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, default='data')# /wine-quality-red')
-    parser.add_argument("--hyper", type=str, default='hyper')
-    parser.add_argument("--n_split", type=int, default=10)
-    parser.add_argument("--n_iter", type=int, default=10)
+    parser.add_argument("--data", type=str, default='../data/cleveland')
+    parser.add_argument("--hyper", type=str, default='../test3/hyper/cleveland')
+    parser.add_argument("--n_split", type=int, default=3)
+    parser.add_argument("--n_iter", type=int, default=3)
     parser.add_argument("--log", type=str, default='log')
     parser.add_argument("--dir", type=int, default=0)
     args = parser.parse_args()
-    tools.start_log(args.log)
+#    tools.start_log(args.log)
+    alpha_optim(args.data,args.hyper,args.n_split,args.n_iter,5)
