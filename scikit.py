8c593de1ace25c499f9683d9ebdd554fc56c3284
tjacek <tjacek@student.agh.edu.pl>
2023-07-24 20:59:54 +0200

    clone scikit

--- scikit.py
+++ scikit.py
@@ -2,11 +2,14 @@ import tools
 tools.silence_warnings()
 import argparse
 import numpy as np
+import sklearn
+raise Exception(sklearn.__version__)
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.model_selection import GridSearchCV
 from skopt import BayesSearchCV
 from skopt.space import Real, Categorical, Integer
 from sklearn.base import BaseEstimator, ClassifierMixin
+from time import time
 import pandas as pd
 import data,deep,learn,train
 
@@ -39,6 +42,11 @@ class ScikitAdapter(BaseEstimator, ClassifierMixin):
         prob=self.predict_proba(X)
         return np.argmax(prob,axis=1)
 
+    def __sklearn_clone__(self):
+        print('clone')
+        return ScikitAdapter(self.alpha,self.hyper)
+#        raise Exception('clone')
+
 class BayesCallback(object):
     def __init__(self):
         self.count=0
@@ -54,7 +62,10 @@ def single_exp(data_path,hyper_path,n_split,n_repeats):
     print(data_path)
     print(hyper_path)
     X,y=data.get_dataset(data_path)
-    hyper_dict=train.parse_hyper(hyper_path)
+    hyper_dict=parse_hyper(hyper_path)
+
+    test_clone(hyper_dict,n_iter=10)
+
     cv_gen=RepeatedStratifiedKFold(n_splits=n_split, 
                                    n_repeats=n_repeats, 
                                    random_state=1)
@@ -68,7 +79,6 @@ def single_exp(data_path,hyper_path,n_split,n_repeats):
     return best_params['alpha'],best_score
 
 def multi_exp(args,out_path):
-#    raise Exception(out_path)
     for path_i in tools.top_files(args.data):
         with open(out_path,"a") as f:
             name_i=path_i.split('/')[-1]    
@@ -98,11 +108,41 @@ def bayes_search(cv_gen,hyper_dict,n_iter=5):
                            n_jobs=1)
     return search
 
+
+def test_clone(hyper_dict,n_iter=10):
+    import inspect
+    from sklearn.base import clone
+    build_time,scikit_obj=[],[]
+    for i in range(n_iter):
+        start=time()
+        scikit_clf_i=ScikitAdapter(0.5,hyper_dict)
+        scikit_obj.append(scikit_clf_i)
+        build_time.append(time()-start)
+    print(f'{np.mean(build_time):.4}')
+    clone_time,clonned_obj=[],[]
+    for scikit_clf_i in scikit_obj:
+        start=time()
+        clonned_obj.append( clone(scikit_clf_i))
+        clone_time.append(time()-start)
+    print(f'{np.mean(clone_time):.4}')
+    raise Exception('OK')
+
+def parse_hyper(hyper_path):
+    with open(hyper_path) as f:
+        line = eval(f.readlines()[-1])
+        hyper_dict=line[0]
+        layers= [key_i for key_i in hyper_dict
+                   if('unit' in key_i)]
+        layers.sort()
+        return { 'batch':hyper_dict['batch'],
+                 'layers':[hyper_dict[name_j] 
+                            for name_j in layers] }
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, default='../data')
-    parser.add_argument("--hyper", type=str, default='../test3/hyper')
-    parser.add_argument("--n_split", type=int, default=3)
+    parser.add_argument("--data", type=str, default='../r_uci')
+    parser.add_argument("--hyper", type=str, default='../10_10/hyper')
+    parser.add_argument("--n_split", type=int, default=10)
     parser.add_argument("--n_iter", type=int, default=3)
     parser.add_argument("--out_path", type=str, default='alpha.csv')
     parser.add_argument("--log", type=str, default='log')
