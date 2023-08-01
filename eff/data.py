0315cfaa74b3946fa2749f91e73f02693da4376e
tjacek <tjacek@agh.edu.pl>
2023-08-01 23:50:34 +0200

    get splits

--- eff/data.py
+++ eff/data.py
@@ -1,12 +1,22 @@
 import numpy as np
 from sklearn import preprocessing
 from collections import Counter#defaultdict
+from sklearn.model_selection import RepeatedStratifiedKFold
 
 class Dataset(object):
     def __init__(self,X,y,cs=None):
         self.X=X
         self.y=y
         self.cs=cs 
+
+    def get_splits(self,n_splits,n_repeats):
+        cv = RepeatedStratifiedKFold(n_splits=n_splits, 
+                                     n_repeats=n_repeats, 
+                                     random_state=4)
+        all_splits=[]
+        for train_i,test_i in cv.split(self.X,self.y):
+            all_splits.append((train_i,test_i))
+        return all_splits
 
     def get_params(self):
         return {'n_cats':max(self.y)+1,
@@ -17,7 +27,7 @@ class Dataset(object):
 def prepare_data(df):
     X=df.iloc[:,:-1]
     X=X.to_numpy()
-    X=preprocessing.scale(X) # data preprocessing
+    X=preprocessing.scale(X)
     y=df.iloc[:,-1]
     cats={ cat_i:i for i,cat_i in enumerate(y.unique())}
     y=y.to_numpy()
