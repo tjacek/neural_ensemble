0a57f49634c4d47f421173a54d048e4ad7f0e142
tjacek <tjacek@agh.edu.pl>
2023-08-02 00:53:46 +0200

    eff ensemble

---
+++ eff/deep.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+import keras
+from sklearn import preprocessing
+from tensorflow.keras.layers import Dense,BatchNormalization,Concatenate
+from tensorflow.keras import Input, Model
+import data
+
+def nn_builder(params,hyper_params,n_splits=10):
+    outputs,inputs=[],[]
+    for i in range(n_splits):
+        input_layer = Input(shape=(params['dims']))
+        inputs.append(input_layer)
+        x_i=input_layer
+        for j,hidden_j in enumerate(hyper_params['layers']):
+            x_i=Dense(hidden_j,activation='relu',
+                    name=f"layer_{i}_{j}")(x_i)
+        if(hyper_params['batch']):
+            x_i=BatchNormalization(name=f'batch_{i}')(x_i)
+        x_i=Dense(params['n_cats'], activation='softmax',name=f'out_{i}')(x_i)
+        outputs.append(x_i)
+    return Model(inputs=inputs, outputs=outputs)
+
+#def train(in):
+
+if __name__ == "__main__":
+    in_path='../../uci/cleveland'
+    full=data.get_dataset(in_path)
+    params=full.get_params()
+    hyper_params={'layers':[20,20],'batch':True}
+    model=nn_builder(params,hyper_params,n_splits=10)
+    model.summary()
