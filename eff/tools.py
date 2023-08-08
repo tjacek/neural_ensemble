5483ca1d50d4d5c294a0ced1c95694607b5e2cc6
tjacek <tjacek@agh.edu.pl>
2023-08-08 19:21:33 +0200

    log train

--- eff/tools.py
+++ eff/tools.py
@@ -2,6 +2,7 @@ import os,warnings
 import logging,time
 import re
 from sklearn.metrics import accuracy_score
+from functools import wraps
 
 def get_metric(metric_type):
     if(metric_type=='acc'):
@@ -47,3 +48,38 @@ def atof(text):
 
 def natural_keys(text):
     return [ atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', text) ]
+
+def dir_fun(n_paths=1):
+    def helper(fun):
+        @wraps(fun)
+        def decor_fun(*args, **kwargs):
+            path_args=args[:n_paths]
+            make_dir(path_args[-1])
+            path_dir={}
+            for path_i in top_files(path_args[0]):
+                name_i=path_i.split('/')[-1]
+                new_args=list(args)
+                for j,arg_j in enumerate(path_args):
+                    new_args[j]=f'{arg_j}/{name_i}'
+                path_dir[path_i]= fun(*new_args,**kwargs)
+            return path_dir
+        return decor_fun
+    return helper
+
+def log_time(task='TRAIN'):
+    def helper(fun):
+        @wraps(fun)
+        def decor_fun(*args, **kwargs):
+            name_i=args[0].split('/')[-1]
+            start=time.time()
+            result=fun(*args,**kwargs)
+            diff=(time.time()-start)
+            logging.info(f'{task}-{name_i}-{diff:.4f}s')
+            return result
+        return decor_fun
+    return helper
+
+def start_log(log_path):
+    logging.basicConfig(filename=log_path,#'{}/time.log'.format(dir_path), 
+        level=logging.INFO,filemode='a', 
+        format='%(process)d-%(levelname)s-%(message)s')
--- eff/train.py
+++ eff/train.py
@@ -2,33 +2,41 @@ import tools
 tools.silence_warnings()
 import pandas as pd
 from keras import callbacks
-import argparse
+import os,argparse
 import data,deep,ens,learn,tools
 
 def train_exp(data_path,hyper_path,model_path,n_splits=10,n_repeats=10):
-    dataset=data.get_dataset(data_path)
-    all_splits=dataset.get_splits(n_splits=n_splits,
-                                  n_repeats=n_repeats)
-    params=dataset.get_params()
+
     hyper_df=pd.read_csv(hyper_path)
-    name_i=data_path.split('/')[-1]
-    hyper_dict=tools.get_hyper(name_i,hyper_df)
+
     early_stop = callbacks.EarlyStopping(monitor='accuracy',
                                          mode="max", 
                                          patience=5,
                                          restore_best_weights=True)
-    tools.make_dir(model_path)
-    for i,split_i in enumerate(all_splits):
-        deep_ens=ens.build_multi(params=params,
+    @tools.log_time(task='TRAIN')
+    def helper(data_path,model_path):
+        dataset=data.get_dataset(data_path)
+        all_splits=dataset.get_splits(n_splits=n_splits,
+                                      n_repeats=n_repeats)
+        params=dataset.get_params()
+        name_i=data_path.split('/')[-1]
+        print(name_i)
+        hyper_dict=tools.get_hyper(name_i,hyper_df)
+        tools.make_dir(model_path)
+        for i,split_i in enumerate(all_splits):
+            deep_ens=ens.build_multi(params=params,
                                      hyper_params=hyper_dict,
                                      split=split_i)
-        deep_ens.fit(x=dataset.X,
-                     y=dataset.y,
-                     epochs=150,
-                     batch_size=params['batch'],
-                     verbose=1,
-                     callbacks=early_stop)
-        deep_ens.save(f'{model_path}/{i}')
+            deep_ens.fit(x=dataset.X,
+                         y=dataset.y,
+                         epochs=150,
+                         batch_size=params['batch'],
+                         verbose=1,
+                         callbacks=early_stop)
+            deep_ens.save(f'{model_path}/{i}')
+    if(os.path.isdir(data_path)):
+        helper=tools.dir_fun(2)(helper)
+    helper(data_path,model_path)
 
 def pred_exp(data_path,hyper_path,model_path):
     dataset=data.get_dataset(data_path)
@@ -59,14 +67,16 @@ def extract_exp(data_path,hyper_path,model_path):
 if __name__ == '__main__':
     dir_path='../../optim_alpha/s_10_10'
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, default='../../s_uci/cleveland')
-    parser.add_argument("--hyper", type=str, default=f'{dir_path}/hyper.csv')
-    parser.add_argument("--models", type=str, default=f'models2')
+    parser.add_argument("--data", type=str, default='../../s_uci')
+    parser.add_argument("--hyper", type=str, default=f'../../hyper.csv')
+    parser.add_argument("--models", type=str, default=f'models')
     parser.add_argument("--n_splits", type=int, default=10)
     parser.add_argument("--n_repeats", type=int, default=10)
+    parser.add_argument("--log", type=str, default=f'log.info')
     args = parser.parse_args()
-    extract_exp(data_path=args.data,
+    tools.start_log(args.log)
+    train_exp(data_path=args.data,
                 hyper_path=args.hyper,
-                model_path=args.models)#,
-#              n_splits=args.n_splits,
-#              n_repeats=args.n_repeats)
+                model_path=args.models,
+                n_splits=args.n_splits,
+                n_repeats=args.n_repeats)
