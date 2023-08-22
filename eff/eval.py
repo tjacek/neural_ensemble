de912c4c072b893f95ab1c4ad8300440caf04040
tjacek <tjacek@agh.edu.pl>
2023-08-22 09:41:05 +0200

    is_pred dir

--- eff/eval.py
+++ eff/eval.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import os,argparse
 from collections import defaultdict
-import pred
+import pred,tools
 
 class MetricDict(defaultdict):
     def __init__(self):#, arg=[]):
@@ -47,9 +47,26 @@ def get_id(path_i):
     raw=path_i.split('/')
     return f'{raw[-3]}-{raw[-1]}'
 
+
+def is_pred_dir(dir_path):
+    for path_i in tools.top_files(dir_path):
+        if(not path_i.split('/')[-1].isnumeric()):
+            return False
+    return True
+
+def eval_exp(pred_path):
+    def helper(pred_path):
+        print(pred_path)
+        metric_dict= read_metric_dict(pred_path)
+        metric_dict.to_df()
+    print(is_pred_dir(pred_path))
+#    if(os.path.isdir(pred_path)):
+#        helper=tools.dir_fun(3)(helper)
+#    helper(pred_path)
+
 if __name__ == '__main__':
+    dir_path='../../s_10_10'
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pred", type=str, default=f'pred')
+    parser.add_argument("--pred", type=str, default=f'{dir_path}/pred/binary/cleveland')
     args = parser.parse_args()
-    metric_dict= read_metric_dict(args.pred)
-    metric_dict.to_df()
+    eval_exp(args.pred)
