5357726f96aafaaef1515d2b1f88129283934e52
tjacek <tjacek@agh.edu.pl>
2023-09-18 11:08:46 +0200

    logistic regression

--- summary/reg.py
+++ summary/reg.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd 
+from sklearn.linear_model import LogisticRegression
 import exp
 
 if __name__ == '__main__':
@@ -15,7 +17,14 @@ if __name__ == '__main__':
     
     df=pd.concat(pvalue_df)
     df['delta']=df['diff']
-    df['sig_better']=df.apply(lambda x: (x.sig>0 and x.delta>0), 
+    df['sig_better']=df.apply(lambda x: int(x.sig>0 and x.delta>0), 
     	                      axis=1)
     df=df[['dataset','sig_better']]
-    print(df)
+    df= stats_pd.merge(df, on='dataset')
+    X=df[['classes','samples','features','gini']].to_numpy()
+    y=df['sig_better'].to_numpy()
+    
+    clf=LogisticRegression(solver='liblinear')#,
+#            class_weight='balanced')
+    clf.fit(X,y)
+    print(clf.coef_ /np.sum(np.abs(clf.coef_)))
