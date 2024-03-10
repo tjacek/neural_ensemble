cc663df83f85ff406c068aa73a644a1867b0c275
tjacek <tjacek@agh.edu.pl>
2024-03-10 19:24:24 +0100

    stats

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import utils
 utils.silence_warnings()
 import base,data,exp,deep
+import numpy as np
+from scipy import stats
 
 def train_data(dataset,protocol,alg_params,hyper_params,verbose=0):
     acc=[]
@@ -15,7 +17,7 @@ def train_data(dataset,protocol,alg_params,hyper_params,verbose=0):
         acc.append(result_i.acc()-result.acc())
     print(acc)
 
-def stat_sig(dataset,protocol,alg_params,hyper_params,clf_type="RF"):
+def stat_sig(dataset,protocol,alg_params,hyper_params,clf_type="RF",verbose=0):
     rf_results,ne_results=[],[]
     for split_i in protocol.iter(dataset):
         exp_i=exp.make_exp(split_i,hyper_params)
@@ -23,14 +25,22 @@ def stat_sig(dataset,protocol,alg_params,hyper_params,clf_type="RF"):
         	        verbose=0)
         ne_results.append(  exp_i.eval(alg_params))        
         rf_results.append(exp_i.split.eval(clf_type))
+    pvalue,clf_mean,ne_mean=compute_pvalue(rf_results,ne_results)
+    print(f"{pvalue:.3f},{clf_mean:.3f},{ne_mean:.3f}")
+
+def compute_pvalue(clf_results,ne_results):
+    clf_acc=[result_i.acc() for result_i in clf_results]
+    ne_acc=[result_i.acc() for result_i in ne_results]
+    pvalue=stats.ttest_ind(clf_acc,ne_acc,equal_var=False)[1]
+    return pvalue,np.mean(clf_acc),np.mean(ne_acc)
 
 if __name__ == '__main__':
     in_path='../uci/cleveland'
     dataset=data.get_data(in_path)
 #    hyper_params={'units_0': 204, 'units_1': 52, 'batch': 0, 'layers': 2}
     hyper_params={'units_0': 123, 'units_1': 65, 'batch': 0, 'layers': 2}
-    hyper_dict=train_data(dataset=dataset,
-                          protocol=base.Protocol(n_split=3,n_iters=3),
+    hyper_dict=stat_sig(dataset=dataset,
+                          protocol=base.Protocol(n_split=10,n_iters=10),
                           alg_params=base.AlgParams(),
                           hyper_params=hyper_params,
                           verbose=0)
