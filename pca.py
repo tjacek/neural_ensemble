1fa57c8afd840b45efd080b69bd13ff996837caa
tjacek <tjacek@agh.edu.pl>
2024-05-10 22:37:11 +0200

    make_split

--- pca.py
+++ pca.py
@@ -9,23 +9,27 @@ class DeoractorPCA(protocol.ExpIO):
         cs= exp_ij.make_extractor()
         for cs_i in cs:
             feats_i=np.concatenate([dataset.X,cs_i],axis=1)
-            data_i=data.Dataset(X=feats_i,
-                                y=dataset.y,
-                                params=dataset.params)
-            split_i=protocol.Split(dataset=data_i,
-                                   train=exp_ij.train,
-                                   test=exp_ij.test)
+            split_i= make_split(feats_i,dataset,exp_i):
             all_splits.append(split_i)
         return base.NECSCF(all_splits=all_splits)
+
+def make_split(feats_i,dataset,exp_i):
+    data_i=data.Dataset(X=feats_i,
+                        y=dataset.y,
+                        params=dataset.params)
+    return protocol.Split(dataset=data_i,
+                           train=exp_i.train,
+                            test=exp_i.test)
 
 @utils.DirFun([("data_path",0),("model_path",1)])
 def stat_sig(data_path:str,
              model_path:str,
              protocol_obj:protocol.Protocol,
              clf_type="RF"):
-    protocol_obj.io_type= DeoractorPCA(protocol_obj.io_type)
+#    protocol_obj.io_type= DeoractorPCA(protocol_obj.io_type)
     dataset=data.get_data(data_path)
     exp_io= protocol_obj.get_group(exp_path=model_path)
+    exp_io=DeoractorPCA(exp_io)
     for nescf_ij in exp_io.iter_necscf(dataset):
     	print(nescf_ij)
 
