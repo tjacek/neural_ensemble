ffc1ed16622ae72e6b771e81d6f5cb18578c8e6b
tjacek <tjacek@agh.edu.pl>
2024-06-15 23:05:58 +0200

    fun arg

--- eval.py
+++ eval.py
@@ -66,16 +66,16 @@ def indiv_acc(data_path:str,
             print(mean_acc)
     print(mean_acc)
 
-def multiple_exp(args,prot):
+def multiple_exp(args,prot,fun=None):
+    if(fun is None):
+        fun=indiv_acc
     if(args.multi):
         arg_names=[("data_path",0),("model_path",1)]
-        exp=utils.DirFun(arg_names)(indiv_acc)
-    else:
-        exp=indiv_acc
-    r_dict=exp(data_path=args.data,
+        fun=utils.DirFun(arg_names)(fun)
+    r_dict=fun(data_path=args.data,
                model_path=args.model,
-                     protocol_obj=prot)
-    print(r_dict)
+               protocol_obj=prot)
+    utils.print_dict(r_dict)
 
 if __name__ == '__main__':
     parser =  utils.get_args(['data','model'],
@@ -86,8 +86,3 @@ if __name__ == '__main__':
                            split_gen=protocol.SplitGenerator(n_split=args.n_split,
                                                              n_iters=args.n_iter))
     multiple_exp(args,prot)
-#    r_dict=indiv_acc(data_path=args.data,
-#                    model_path=args.model,
-#                    protocol_obj=prot)
-#    print(r_dict)
-#    utils.print_dict(r_dict)
