5d1eb4fba0c9a8c1b63ff08b88f7833cc81ee622
tjacek <jacekt89@gmail.com>
2025-07-27 13:57:55 +0200

    cs extractor

--- hyper.py
+++ hyper.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn import tree
-import base
+import base,dataset
 
 class TreeFeatClf(object):
     def __init__(self,extract_feats,
@@ -9,20 +9,17 @@ class TreeFeatClf(object):
         self.extract_feats=extract_feats
         self.clf_type=clf_type
         self.concat=concat
-        self.tree_feats=None
         self.clf=None
 
     def fit(self,X,y):
-#        tree=self.tree_factory()
-#        tree.fit(X,y)
-        self.tree_feats=self.extract_feats(X,y) #make_tree_feats(tree)
+        self.extract_feats.fit(X,y)
         self.clf=base.get_clf(self.clf_type)
-        X=self.tree_feats(X=X,
-                          concat=self.concat)
+        X=self.extract_feats(X=X,
+                             concat=self.concat)
         self.clf.fit(X,y)
 
     def predict(self,X):
-        X=self.tree_feats(X=X,
+        X=self.extract_feats(X=X,
                           concat=self.concat)
         return self.clf.predict(X)
 
@@ -49,28 +46,6 @@ class TreeFeatures(object):
             new_feats.append(int(value_i<thres_i) )
         return np.array(new_feats)
 
-class FeatureExtractor(object):
-    def __init__(self,tree_factory=None):
-        if(tree_factory is None):
-            tree_factory=gradient_tree
-        self.tree_factory=tree_factory
-
-    def __call__(self,X,y):
-        tree=self.tree_factory()
-        tree.fit(X,y)
-        return make_tree_feats(tree)
-        
-class SpecificTreeFeatures(object):
-    def __init__(self,specifc_trees):
-        self.specifc_trees=specifc_trees
-
-    def __call__(self,X):
-        feats=[tree_i(X,concat=False) 
-                  for tree_i in self.specifc_trees]
-        feats=np.concatenate(X,axis=0)
-        raise Exception(feats.shape)
-        return feats
-
 def make_tree_feats(tree):
     raw_tree=tree.tree_.__getstate__()['nodes']
     feats,thres=[],[]
@@ -81,19 +56,59 @@ def make_tree_feats(tree):
             thres.append(node_i[3])
     return TreeFeatures(feats,thres)
 
+class FeatureExtractor(object):
+    def __init__(self,tree_factory=None):
+        if(tree_factory is None):
+            tree_factory=gradient_tree
+        self.tree_factory=tree_factory
+        self.tree_feats=None
+
+    def fit(self,X,y):
+        tree=self.tree_factory()
+        tree.fit(X,y)
+        self.tree_feats=make_tree_feats(tree)
+
+    def __call__(self,X,concat=True):
+        return self.tree_feats(X,concat)
+        
+class CSExtractor(object):
+    def __init__(self,tree_factory=None):
+        if(tree_factory is None):
+            tree_factory=gradient_tree
+        self.tree_factory=tree_factory
+        self.indiv__feats=[]
+
+    def fit(self,X,y):
+        data= dataset.Dataset(X,y)
+        for i in range(data.n_cats()):
+            data_i=data.binarize(i)
+            tree_i=self.tree_factory()
+            tree_i.fit(X=data_i.X,
+                       y=data_i.y)
+            feats_i=make_tree_feats(tree_i)
+            self.indiv__feats.append(feats_i)
+
+    def __call__(self,X,concat=True):
+        feats=[feats_i(X,concat=False) 
+                  for feats_i in self.indiv__feats]
+        if(concat):
+            feats.append(X)
+        feats=np.concatenate(feats,axis=1)
+        return feats
+
 def gradient_tree():
     return tree.DecisionTreeClassifier(max_depth=3,
                                        class_weight="balanced")
 
 def eval_features(in_path):
     def helper():
-        return TreeFeatClf(tree_factory=gradient_tree,
-                           extract_feats=make_tree_feats,
+        return TreeFeatClf(extract_feats=CSExtractor(),
                            clf_type="LR",
-                           concat=False)
-    clf_dict={"TREE":gradient_tree,
+                           concat=True)
+    clf_dict={
+              "TREE":gradient_tree,
               "TREE-SVM":helper,
-              "SVM":"LR"}
+              "SVM":"SVM"}
     compare_clf(in_path,clf_dict)
 
 def compare_clf(in_path,clf_dict):
