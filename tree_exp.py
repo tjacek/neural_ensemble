fccb31e583a4a3738ae2122c1c2aa8cde2bec1f2
tjacek <jacekt89@gmail.com>
2025-08-01 13:50:30 +0200

    tree_exp

---
+++ tree_exp.py
@@ -0,0 +1,34 @@
+import numpy as np
+import base
+import tree_feats,utils
+utils.silence_warnings()
+
+def compare_exp(in_path,clfs):
+    data_split=base.get_splits(data_path=in_path,
+                               n_splits=10,
+                               n_repeats=1)
+    for clf_type_i in clfs:
+        desc_i,clf_factory_i=get_clf(clf_type_i)
+        acc_i,balance_i=[],[]
+        for clf_j,result_j in data_split.eval(clf_factory_i):
+            print(clf_j)
+            acc_i.append(result_j.get_acc())
+            balance_i.append(result_j.get_metric("balance"))
+        print(f"{desc_i}:{np.mean(acc_i):.4f},{np.mean(balance_i):.4f}")
+
+
+def get_clf(clf_factory):
+    if(type(clf_factory)==str):
+        desc=clf_factory
+        clf_factory=base.ClasicalClfFactory(clf_factory)
+        return desc,clf_factory
+    desc=str(clf_factory)
+    return desc,clf_factory
+
+clfs=[ tree_feats.GradientTree(),
+       tree_feats.RandomTree(),
+       "LR",
+       "SVM"]
+
+compare_exp(in_path="bad_exp/data/wine-quality-red",
+	        clfs=clfs[:2])
