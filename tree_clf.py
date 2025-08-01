39b979bd3c8ad8a4110e0fdacb70973e14cf0dbe
tjacek <jacekt89@gmail.com>
2025-08-01 10:56:32 +0200

    get_path

--- tree_clf.py
+++ tree_clf.py
@@ -1,20 +1,46 @@
 import numpy as np
+from scipy.stats import entropy 
 import tree_feats
 
 class TreeDict(dict):
+    def __len__(self):
+        return len(self["feat"])
 
     def get_node(self,i):
-        keys=self.keys()
+        keys=list(self.keys())
         keys.sort()
-        if(type(i)==int):
+        print(keys)
+        if(type(i)!=list):
             return [self[key_j][i] for key_j in keys]
         return [[self[key_j][k] for key_j in keys]
                          for k in i]
-    def show(self,i):
-        value_i=clf.tree_.value[i]
-        samples_i=clf.tree_.weighted_n_node_samples[i]
-        print(samples_i)
-        print(value_i)
+    
+    def get_attr(self,key,nodes):
+        return [self[key][i] for i in nodes]
+
+    def mutual_info(self):
+        dist=tree_dict["value"][0]
+        n_samples=len(self)
+        offset=np.ones(dist.shape)
+        h_y=entropy(dist)
+        mi=[]
+        for i,value_i in enumerate(self["value"]):
+            samples_i=self["samples"][i]
+            value_i=self["value"][i]
+            p_y= samples_i/ n_samples
+            h_yx=p_y*entropy(value_i)#,nan_policy='omit')
+            h_yx+=(1-p_y)*entropy(offset-value_i)
+            i_xy=h_y-h_yx
+            mi.append(i_xy)
+        return mi
+
+    def get_path(self,i):
+        path=[i]
+        while(i>=0):
+            print(type(i))
+            i=self["parent"][i]
+            path.append(i)
+        return path
 
 def make_tree_dict(clf):
     tree_dict=TreeDict()
@@ -26,14 +52,21 @@ def make_tree_dict(clf):
                             for value_i in clf.tree_.value]
     tree_dict["samples"]=clf.tree_.weighted_n_node_samples
     n_nodes=len(tree_dict["feat"])
-    tree_dict["parents"]= -np.ones((n_nodes,))
+    tree_dict["parent"]= -np.ones((n_nodes,),dtype=int)
     for i in range(n_nodes):
         left_i=tree_dict["left"][i]
         right_i=tree_dict["right"][i]
         if(left_i>=0):
-            tree_dict["parents"][left_i]=i
-            tree_dict["parents"][right_i]=i
+            tree_dict["parent"][left_i]=i
+            tree_dict["parent"][right_i]=i
     return tree_dict
+
+
+def inf_features(tree_dict):
+    mutual_info=tree_dict.mutual_info()
+    index=np.argsort(mutual_info)
+    print(tree_dict.get_node(index[0]))
+    print(tree_dict.get_path(index[0]))
 
 if __name__ == '__main__':
     import base,dataset
@@ -42,6 +75,7 @@ if __name__ == '__main__':
     clf=tree_feats.get_tree("random")()
     clf.fit(data.X,data.y)
 #    raise Exception(clf.tree_.__getstate__()['nodes'])
-    make_tree_dict(clf)
-#    facade=TreeFacade(clf)
-#    print(facade)
+    tree_dict=make_tree_dict(clf)
+    inf_features(tree_dict)
+#    print(tree_dict.get_node(index[:10])[0])
+#    print(tree_dict.get_attr("feat",index[:10]))
