0e496c5e8770317e1c0f19fc77945a721e414ecd
tjacek <jacekt89@gmail.com>
2025-08-04 17:50:00 +0200

    clf_compare

--- hyper.py
+++ hyper.py
@@ -1,6 +1,7 @@
 import numpy as np
 #from sklearn.decomposition import PCA
 from tqdm import tqdm
+from itertools import product
 import base,clfs,utils#dataset,tree_feats
 utils.silence_warnings()
 
@@ -14,6 +15,20 @@ def hyper_comp(in_path,hyper):
     for hyper_i in hyper:
         eval_tree(data_split,hyper_i,tree_i)
 
+def tree_comp(in_path):
+    data_split=base.get_splits(data_path=in_path,
+                               n_splits=10,
+                               n_repeats=1)
+    prototype={"clf_factory":"random",
+                "concat":True}
+    extr=["info","ind"]
+    n_feats=[20,30,50]
+    hyper={'layers':2, 'units_0':1,'units_1':1,'batch':False}
+    for extr_i in product(extr,n_feats):
+        tree_i=prototype.copy()
+        tree_i["extr_factory"]=extr_i
+        eval_tree(data_split,hyper,tree_i)
+
 def eval_tree(data_split,hyper_i,tree_i):
     nn_factory_i=clfs.TreeMLPFactory(hyper_i,tree_i)
     nn_factory_i.init(data_split.data)
@@ -21,6 +36,7 @@ def eval_tree(data_split,hyper_i,tree_i):
     for clf_j,result_j in tqdm(data_split.eval(nn_factory_i)):
         acc_i.append(result_j.get_acc())
         balance_i.append(result_j.get_metric("balance"))
+    print(tree_i)
     print(hyper_i)
     print(f"{np.mean(acc_i):.4f},{np.mean(balance_i):.4f}")
 
@@ -30,4 +46,6 @@ if __name__ == '__main__':
            {'layers':2, 'units_0':1,'units_1':1,'batch':False},
            {'layers':2, 'units_0':2,'units_1':1,'batch':True},
            {'layers':2, 'units_0':1,'units_1':1,'batch':True},]
-    hyper_comp("bad_exp/data/wine-quality-red",hyper)
+    in_path="bad_exp/data/wine-quality-red"
+#    hyper_comp(in_path,hyper)
+    tree_comp(in_path)
