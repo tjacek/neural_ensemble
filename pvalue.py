26777c461ce870f26141d0f395862e429f5a191a
tjacek <jacekt89@gmail.com>
2025-08-11 21:44:52 +0200

    plot pvalue

--- pvalue.py
+++ pvalue.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import stats
+import seaborn as sn
+import matplotlib.pyplot as plt
 import dataset,utils
 
 def pvalue_matrix(in_path,clf_type="RF",metric="acc"):
@@ -8,8 +10,9 @@ def pvalue_matrix(in_path,clf_type="RF",metric="acc"):
     all_clfs=list(metric_dict.values())[0].keys()
     other_clfs=[clf_i for clf_i in all_clfs
                     if(clf_i!=clf_type) ]
+    data=list(metric_dict.keys())
     sig_matrix=[]
-    for data_i in metric_dict:
+    for data_i in data:
         metric_i=metric_dict[data_i][clf_type]
         sig_i=[]
         for clf_j in other_clfs:
@@ -21,6 +24,11 @@ def pvalue_matrix(in_path,clf_type="RF",metric="acc"):
             sign_ij= int(pvalue<0.05) * np.sign(diff_i)
             sig_i.append(sign_ij)
         sig_matrix.append(sig_i)
+    sig_matrix=np.array(sig_matrix)
+    print(sig_matrix.shape)
+    heatmap(matrix=sig_matrix,
+            x_labels=other_clfs,
+            y_labels=data)
     print(sig_matrix)
 
 def get_result_dict(in_path):
@@ -40,6 +48,24 @@ def compute_metric(result_dict,metric):
     return { data_i:{name_j:result_j.get_metric(metric) 
                        for name_j,result_j in dict_i.items()}
               for data_i,dict_i in result_dict.items()}
+
+def heatmap(matrix,
+            x_labels,
+            y_labels,
+            title="Statistical significance (RF)"):
+    fig, ax = plt.subplots()
+    ax=sn.heatmap(matrix,
+                  cmap="RdBu_r",
+                  linewidth=0.5,
+                  cbar=False,
+                  ax=ax)
+    y_labels.sort()
+    ax.set_xticklabels(x_labels,rotation = 90)
+    ax.set_yticklabels(y_labels,rotation = 0)
+    ax.set_title(title,fontsize=10)
+    plt.tight_layout()
+    plt.show()
+#    return ax.get_figure()
 
 in_path="uci_exp/exp"
 pvalue_matrix(in_path,metric="acc")
