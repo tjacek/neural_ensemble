f774e33eb81369ca9d9f56fe5b548d35e4ecdb16
tjacek <jacekt89@gmail.com>
2025-09-11 19:13:16 +0200

    select datasets

--- hyper.py
+++ hyper.py
@@ -7,12 +7,12 @@ import base,clfs,utils,tree_clf,tree_feats,dataset
 utils.silence_warnings()
 
 
-def nn_tree(in_path,multi=False):
-    extr=["ind"]#"info","ind","prod"]
-    n_feats=[10,20] #,30,50]
+def nn_tree(in_path,multi=False,selected=None):    
+    if(selected):
+        selected=set(selected)
+    extr=["info","ind","prod"]
+    n_feats=[10,20,30,50]
     units=[1,2]
-#    hyper_params={ 'layers':2, 'units_0':1,
-#                   'units_1':1,'batch':False}
     def arg_iter():
         for feat_i,dim_i in product(extr,n_feats):
             arg_i={ "tree_factory":"random",
@@ -23,16 +23,15 @@ def nn_tree(in_path,multi=False):
                           'units_1':1,'batch':False}
                 yield arg_i,hyper_j
     def helper(in_path):
+        data=in_path.split("/")[-1]
+        print(data)
+        if(selected and not (data in selected)):
+            return []
         data_split=base.get_splits( data_path=in_path,
                                     n_splits=10,
                                     n_repeats=1)
-        data=in_path.split("/")[-1]
         lines=[]
         for arg_i,hyper_i in arg_iter():
-#        for feat_i,dim_i in product(extr,n_feats):
-#            arg_i={ "tree_factory":"random",
-#                    "extr_factory":(feat_i,dim_i),
-#                    "concat":True}
             feat_i,dim_i=arg_i['extr_factory']
             clf_factory=clfs.get_clfs("TREE-MLP",
                                       hyper_params=hyper_i,
@@ -93,14 +92,18 @@ def nn_tree(in_path,multi=False):
 #    return np.mean(acc),np.mean(balance)
 
 
-def svm_tree(in_path,multi=False):
+def svm_tree(in_path,multi=False,selected=None):
+    if(selected):
+        selected=set(selected)
     extr=["info","ind","prod"]
     n_feats=[10,20,30,50]
     def helper(in_path):
+        data=in_path.split("/")[-1]
+        if(selected and not (data in selected)):
+            raise Exception(data)
         data_split=base.get_splits(data_path=in_path,
                                n_splits=10,
                                n_repeats=1)
-        data=in_path.split("/")[-1]
         lines=[]
         for feat_i,dim_i in product(extr,n_feats):
             arg_i={ "tree_factory":"random",
@@ -133,11 +136,14 @@ def svm_tree(in_path,multi=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--in_path", type=str,  default="bad")
-    parser.add_argument("--out_path", type=str, default=None)
-    parser.add_argument("--hyper_path", type=str, default=None)
+    parser.add_argument("--in_path", type=str,  default="uci")
+    parser.add_argument("--out_path", type=str, default="uci_out")
+    parser.add_argument("--hyper_path", type=str, default="uci_neural.csv")
     args = parser.parse_args()
-    df=nn_tree(f"{args.in_path}_exp/data",multi=True)
+    print(args)
+    df=nn_tree(f"{args.in_path}_exp/data",multi=True,
+                  selected=["cleveland","cmc",
+                            "dermatology","led7digit"])
     if(args.out_path):
         df.df.to_csv(args.out_path, sep=',')
     if(args.hyper_path):
