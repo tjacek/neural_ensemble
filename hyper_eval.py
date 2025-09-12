97432a551bd3f27181924d83df0aabaaaf4af3f6
tjacek <jacekt89@gmail.com>
2025-09-12 15:27:19 +0200

    best params

--- hyper_eval.py
+++ hyper_eval.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import dataset
 
+def best_param(in_path,metric="accuracy"):
+    df=pd.read_csv(in_path)
+    grouped=df.groupby(by='data')
+    def helper(df_i):
+        df_i=df_i.sort_values(by=metric,ascending=False)
+        worst=df_i.iloc[-1]
+        df_i["worst"]=worst[metric]
+        return df_i.iloc[0]
+    return grouped.apply(helper)
+
 in_path="neural/multi.csv"
-df=pd.read_csv(in_path)
-grouped=df.groupby(by='data')
-metric="accuracy"
-def helper(df_i):
-    df_i=df_i.sort_values(by=metric,ascending=False)
-    worst=df_i.iloc[-1]
-    df_i["worst"]=worst[metric]
-#    print(acc_i)
-    return df_i.iloc[0]
-df=grouped.apply(helper)
+df=best_param(in_path,metric="accuracy")
 print(df)
