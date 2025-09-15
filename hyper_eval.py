eaf2400c11419ed53a794e5dba2111fe4c590223
tjacek <jacekt89@gmail.com>
2025-09-15 21:37:12 +0200

    var params

--- hyper_eval.py
+++ hyper_eval.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import dataset
 
@@ -11,6 +12,17 @@ def best_param(in_path,metric="accuracy"):
         return df_i.iloc[0]
     return grouped.apply(helper)
 
-in_path="neural/multi.csv"
-df=best_param(in_path,metric="accuracy")
+def var_param(in_path,metric="accuracy"):
+    df=pd.read_csv(in_path)
+    grouped=df.groupby(by='data')
+    def helper(df_i):
+        metric_i=df_i[metric]
+        mean_i=np.mean(metric_i)
+        std_i=np.std(metric_i)
+        data_i=df_i['data'].to_list()[0]
+        return pd.Series([data_i,mean_i,std_i])
+    return grouped.apply(helper)
+
+in_path="neural/uci/raw_hyper.csv"
+df=var_param(in_path,metric="accuracy")
 print(df)
