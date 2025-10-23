f463078c2d5c75479655f67caa1496f3cb47f333
tjacek <jacekt89@gmail.com>
2025-10-23 18:08:24 +0200

    splits gen

--- incr_train.py
+++ incr_train.py
@@ -5,14 +5,12 @@ def incr_train(in_path,split_path="split",n=5):
     @utils.DirFun("in_path","exp_path")
     def helper(in_path,exp_path):
         data=dataset.read_csv(in_path)
-        split_path=f"{exp_path}/splits"
-        paths=utils.top_files(split_path)[:10]
+      
         clf_factory=clfs.get_clfs(clf_type=f'TREE-ENS({n})',
                     	          hyper_params=None,
                                   feature_params=None)
         clf_factory.init(data)
-        for i,split_path_i in tqdm(enumerate(paths)):
-            split_i=base.read_split(split_path_i)
+        for split_i in tqdm(splits_gen(exp_path)):
             clf_i=clf_factory()
             clf_i,history_i=split_i.fit_clf(data,clf_i)
             print(split_i)	
@@ -21,10 +19,15 @@ def incr_train(in_path,split_path="split",n=5):
 #	    print(dir_proxy)
     helper(in_path,"bad_exp/exp")
 
-#	for i,split_path_i in tqdm(enumerate(split_path)):
-#            clf_i=clf_factory()
-#            split_i=base.read_split(split_path_i)
-#            clf_i,history_i=split_i.fit_clf(data,clf_i)
+def splits_gen(exp_path,
+               n_splits=10,
+               start=0):
+    split_path=f"{exp_path}/splits"
+    end=start+n_splits
+    paths=utils.top_files(split_path)[start:end]
+    for i,split_path_i in enumerate(paths):
+        split_i=base.read_split(split_path_i)
+        yield start+i,split_i
 
 if __name__ == '__main__':
     in_path="bad_exp/data"
