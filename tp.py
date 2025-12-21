3c8e42df65ebe3751566265c1b975bfa33882016
tjacek <jacekt89@gmail.com>
2025-12-21 11:14:19 +0100

    tp multiple datasets

--- tp.py
+++ tp.py
@@ -5,23 +5,28 @@ from tqdm import tqdm
 import numpy as np
 import base,dataset,utils
 
-def exp(data_path,exp_path):
+def exp(data_path,exp_path,n_splits=30):
     data=dataset.read_csv(data_path)
-    gen=base.splits_gen(exp_path,n_splits=10)
-
+    gen=base.splits_gen(exp_path,n_splits=n_splits)
+    tab_path=f"{exp_path}/TabPF"
+    utils.make_dir(tab_path)
     all_results=[]
     for i,split_i in tqdm(gen):
         clf_i = TabPFNClassifier()
         result_i,history=split_i.eval(data,clf_i)
         all_results.append(result_i)
     result=dataset.ResultGroup(all_results)
+    result.save(f"{tab_path}/results")
     acc=np.mean(result.get_acc())
     print(f"{acc:.4f}")
 
-#name="newthyroid"
-name="wine-quality-red"
-name="lymphography"
-in_path=f"incr_exp/uci/data/{name}"
-exp_path=f"incr_exp/uci/exp/{name}"
-#print(dir(TabPFNClassifier))
-exp(in_path,exp_path)
+def basic_exp():
+    names=["yeast"]
+    data="multi"
+    for name_i in names:
+        in_path=f"incr_exp/{data}/data/{name_i}"
+        exp_path=f"incr_exp/{data}/exp/{name_i}"
+        exp(in_path,exp_path)
+
+clf_i = TabPFNClassifier()
+print(clf_i.model_)
