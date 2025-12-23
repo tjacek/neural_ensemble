6cb7ade0929b36e51136a7ed6701feda8531ac79
tjacek <jacekt89@gmail.com>
2025-12-23 15:19:21 +0100

    TabPFN Factory

--- tp.py
+++ tp.py
@@ -5,14 +5,31 @@ from tqdm import tqdm
 import numpy as np
 import base,dataset,utils
 
+class TabpfFactory(base.AbstractClfFactory):
+    def init(self,data):
+        return self
+
+    def __call__(self):
+        return TabPFNClassifier()
+
+    def read(self,model_path:str):
+        pass
+
+    def get_info(self)->dict:
+        return {"clf_type":"TabPFN","callback":"-",
+                "hyper":"-"}
+    
+
 def exp(data_path,exp_path,n_splits=30):
     data=dataset.read_csv(data_path)
     gen=base.splits_gen(exp_path,n_splits=n_splits)
+    clf_factory=TabpfFactory()
+    clf_factory.init(data)
     tab_path=f"{exp_path}/TabPF"
     utils.make_dir(tab_path)
     all_results=[]
     for i,split_i in tqdm(gen):
-        clf_i = TabPFNClassifier()
+        clf_i = clf_factory()
         result_i,history=split_i.eval(data,clf_i)
         all_results.append(result_i)
     result=dataset.ResultGroup(all_results)
@@ -21,12 +38,11 @@ def exp(data_path,exp_path,n_splits=30):
     print(f"{acc:.4f}")
 
 def basic_exp():
-    names=["yeast"]
-    data="multi"
+    names=["cmc"]
+    data="uci"
     for name_i in names:
         in_path=f"incr_exp/{data}/data/{name_i}"
         exp_path=f"incr_exp/{data}/exp/{name_i}"
         exp(in_path,exp_path)
 
-clf_i = TabPFNClassifier()
-print(clf_i.model_)
+basic_exp()
