c591b5416262623baeb6cba1db4e066a1375a3f0
tjacek <jacekt89@gmail.com>
2025-12-26 19:22:22 +0100

    HyperFile

--- new/hyper.py
+++ new/hyper.py
@@ -1,5 +1,24 @@
+import numpy as np
 from itertools import product
 import base,dataset,tree_clf
+
+class HyperFile(object):
+    def __init__(self,file_path):
+        self.file_path=file_path
+
+    def add_line(self,data_id,param_dict,result):
+        line=[data_id]
+        names=list(param_dict.keys())
+        names.sort()
+        for name_i in names:
+            param_i=param_dict[name_i]
+            line.append(str(param_i))
+        for metric_i in ["acc","balance"]:
+            metric_value_i=result.get_metric(metric_i)
+            line.append(f"{np.mean(metric_value_i):.4f}")
+        print(line)
+        with open(self.file_path, 'a') as file:
+             file.write(",".join(line)+"\n")
 
 class HyperparamSpace(object):
     def __init__( self,
@@ -14,15 +33,17 @@ class HyperparamSpace(object):
                       "n_feats":dim_i}
 
 
-def optim_hyper(in_path):
+def optim_hyper(in_path,out_path):
+    data_id=in_path.split("/")[-1]
     data=dataset.read_csv(in_path)
     splits=base.get_splits(data,10,1)
-    splits=enumerate(splits)
+    hyper_file=HyperFile(out_path)
     hyper_space=HyperparamSpace()
     for hyper_i in hyper_space():
         factory_i=tree_clf.TreeFactory(hyper_i)
-        result=factory_i.get_results(data,splits)
-        print(result)
+        result_i=factory_i.get_results(data,
+                                      enumerate(splits))
+        hyper_file.add_line(data_id,hyper_i,result_i)
 
 in_path="../incr_exp/uci/data/vehicle"
-optim_hyper(in_path)
+optim_hyper(in_path,"hyper.csv")
